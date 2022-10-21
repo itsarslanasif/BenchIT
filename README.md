f231bfd49e4a95bdb8f108ec1a3b0f5c781ad888
Arslan <arslan.asif@devsinc.com>
2022-10-21 08:19:53 -0700

    Started Remvoing chatwoot branding with no shame

---
+++ README.md
@@ -0,0 +1,9 @@
+Ruby 3.0.4
+
+Node 16.10
+
+bundle install
+
+yarn install
+
+foreman start -f Procfile.dev
--- app/javascript/dashboard/constants.js
+++ app/javascript/dashboard/constants.js
@@ -21,6 +21,6 @@ export default {
     CONDENSED: 'condensed',
     EXPANDED: 'expanded',
   },
-  DOCS_URL: '//www.chatwoot.com/docs/product/',
+  DOCS_URL: '//www.benchIT.com/docs/product/',
 };
 export const DEFAULT_REDIRECT_URL = '/app/';
