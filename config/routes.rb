628d18384add848d5405238aa604bcd9813d1286
MuhammadHammadShahzad <hammad.shahzada@devsinc.com>
2022-11-08 15:22:21 +0500

    3rw3rgr | PR Requested changes | User Groups in organizations

--- config/routes.rb
+++ config/routes.rb
@@ -170,8 +170,8 @@ Rails.application.routes.draw do
             resources :articles
           end
 
-          get :get_channels_list, to: "mention#get_channels_list"
-          get :get_users_list, to: "mention#get_users_list"
+          get :get_channels_list, to: 'mention#get_channels_list'
+          get :get_users_list, to: 'mention#get_users_list'
         end
       end
       # end of account scoped api routes
