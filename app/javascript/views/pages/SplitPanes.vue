e6b79ac0a4cf7b64e28e30b033a10b65d5444868
Irfan Nazeer <irfan.nazeer@devsinc.com>
2022-11-10 20:19:56 +0500

    3qtyfd7 | Implemented Split Screen Functionality From Develop Branch In Vue 3

---
+++ app/javascript/views/pages/SplitPanes.vue
@@ -0,0 +1,25 @@
+<template>
+  <splitpanes class="default-theme">
+
+   <pane max-size="33" min-size="10.45" style="background-color:#3f0e40;" ></pane>
+
+   <pane max-size="100" min-size="67"> </pane>
+
+  </splitpanes>
+ </template>
+
+ <script>
+ import { Splitpanes, Pane } from 'splitpanes'
+ import 'splitpanes/dist/splitpanes.css'
+
+ export default {
+   components: { Splitpanes, Pane }
+ }
+ </script>
+
+ <style>
+ splitpanes{
+ height: 100vh;
+ }
+ </style>
+
