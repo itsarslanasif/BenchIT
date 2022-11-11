8c810a2eb2101f67962128b23da4ada6f68bd233
m-adnan-iqbal <107465032+m-adnan-iqbal@users.noreply.github.com>
2022-11-11 19:26:52 +0500

    3qtyh2q-Prosmirror editor implementation

--- app/javascript/entrypoints/front.js
+++ app/javascript/entrypoints/front.js
@@ -3,31 +3,31 @@ const app = createApp(Layout);
 
 import Router from '@/routes.js';
 import Layout from '@/views/shared/layout.vue';
-import Axios from "axios";
+import Axios from 'axios';
 
 // ActionCable setup
 import { createCable } from '@/plugins/cable';
-const Cable = createCable({channel: 'ChatChannel'})
+const Cable = createCable({ channel: 'ChatChannel' });
 
 // From animations + Axios
 import { createApi } from '@/plugins/api';
-const Api = createApi({handler: Axios, namespace: ''});
+const Api = createApi({ handler: Axios, namespace: '' });
 
 // Pinia + Axios setup
 import { createPinia } from 'pinia';
 const Pinia = createPinia();
-Pinia.use(({ store }) => { store.axios = Axios })
+Pinia.use(({ store }) => {
+  store.axios = Axios;
+});
 
 // I18n loader
-import { createI18n } from 'vue-i18n/index'; // Need the /index to avoid warning in console
-const I18n = createI18n({locale: 'current',  messages: translations});
+import { createI18n } from 'vue-i18n'; // Need the /index to avoid warning in console
+const I18n = createI18n({ locale: 'current', messages: translations });
 
-app.use(Router)
-   .use(Pinia)
-   .use(I18n)
-   .use(Api)
-   .use(Cable)
-   .mount('#app')
-
-
-
+app
+  .use(Router)
+  .use(Pinia)
+  .use(I18n)
+  .use(Api)
+  .use(Cable)
+  .mount('#app');
--- app/javascript/views/pages/SplitPanes.vue
+++ app/javascript/views/pages/SplitPanes.vue
@@ -1,25 +1,32 @@
 <template>
   <splitpanes class="default-theme">
+    <pane max-size="33" min-size="10.45" style="background-color:#3f0e40;" />
 
-   <pane max-size="33" min-size="10.45" style="background-color:#3f0e40;" ></pane>
-
-   <pane max-size="100" min-size="67"> </pane>
-
+    <pane max-size="100" min-size="67">
+      <div>
+        <Editor v-model="content" />
+        <textarea v-model="content" />
+      </div>
+    </pane>
   </splitpanes>
- </template>
+</template>
 
- <script>
- import { Splitpanes, Pane } from 'splitpanes'
- import 'splitpanes/dist/splitpanes.css'
+<script>
+import { Splitpanes, Pane } from 'splitpanes';
+import 'splitpanes/dist/splitpanes.css';
+import Editor from './Editor.vue';
 
- export default {
-   components: { Splitpanes, Pane }
- }
- </script>
+export default {
+  components: {
+    Splitpanes,
+    Pane,
+    Editor,
+  },
+};
+</script>
 
- <style>
- splitpanes{
- height: 100vh;
- }
- </style>
-
+<style>
+splitpanes {
+  height: 100vh;
+}
+</style>
