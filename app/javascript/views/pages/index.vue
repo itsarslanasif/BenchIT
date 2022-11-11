cc09d692c6299c69f3dc19a5c3d122de1e7cebb9
Ali Aftab <110173898+ali-aftab-khokhar@users.noreply.github.com>
2022-11-11 14:53:21 +0500

    3UFN813 | Workspace Invite Modal

--- app/javascript/views/pages/index.vue
+++ app/javascript/views/pages/index.vue
@@ -1,9 +1,17 @@
 <template>
   <section class="container">
+    <div>
+      <h3 @click="showModal = !showModal">Modal</h3>
+      <div v-if="showModal" :style="modalStyle">
+        <Modal :modalArray="modalArray" />
+      </div>
+    </div>
     <h1>{{ $t('pages.title') }}</h1>
 
     <p><a href="/dead-link">{{ $t('pages.server_404') }}</a></p>
-    <p><router-link to="/dead-link">{{ $t('pages.client_404') }}</router-link></p>
+    <p>
+      <router-link to="/dead-link">{{ $t('pages.client_404') }}</router-link>
+    </p>
     <p><a @click.prevent="unauthorized" href="#">{{ $t('pages.server_401') }}</a></p>
     <p><a @click.prevent="crash" href="#">{{ $t('pages.server_500') }}</a></p>
     <br />
@@ -11,13 +19,38 @@
   </section>
 </template>
 
-
-
-
 <script>
 import { SampleStore } from "@/stores/sample_store";
+import Modal from '../../widgets/modal.vue'
 
 export default {
+  components: {
+    Modal
+  },
+
+  data() {
+    return {
+      showModal: false,
+      modalArray: [
+        {
+          title: 'Invite People to Workspace',
+          func: this.invitePeople
+        },
+        {
+          title: 'Create a channel',
+          func: this.createChannel
+        },
+        {
+          title: 'Sign Out of Workspace',
+          func: this.signOut
+        }
+      ],
+      modalStyle: {
+        marginBottom: 40,
+      }
+    }
+  },
+
   setup() {
     const store = SampleStore();
 
@@ -30,7 +63,16 @@ export default {
     },
     crash() {
       this.$api.call(this.store.show('this-will-trigger-a-500'));
-    }
+    },
+    signOut() {
+      console.log('Signed Out')
+    },
+    createChannel() {
+      console.log('Create Channel')
+    },
+    invitePeople() {
+      console.log('Invite People')
+    },
   }
 
 }
