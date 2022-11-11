45dedf64e98cc1036cf19507770caac5be40682c
asadtariq-dev <asad.tariq@devsinc.com>
2022-11-11 16:37:10 +0500

    3QTYGHB | Implemented chat pane
    - Added dummy data
    - Added chat compononent

---
+++ app/javascript/views/components/chatPane/data/chats.js
@@ -0,0 +1,16 @@
+const chats = [
+  {
+    id: 1,
+    name: 'Asad',
+    avatar:
+      'https://www.pexels.com/photo/brown-and-black-abstract-painting-1808329/',
+    status: 'online',
+  },
+  {
+    id: 1,
+    name: 'Muhammad Umar',
+    avatar: 'https://ca.slack-edge.com/T0A1N9R2S-U036MH5VA1L-8f5352df2d78-512',
+    isOnline: true,
+    status: 'wfh',
+  },
+];
