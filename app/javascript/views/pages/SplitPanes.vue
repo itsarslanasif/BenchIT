af0606fa3b8318781fb19651b505982433e93bdc
m-adnan-iqbal <107465032+m-adnan-iqbal@users.noreply.github.com>
2022-11-14 19:00:11 +0500

    3qtyh2q resolved indentation issues

--- app/javascript/views/pages/SplitPanes.vue
+++ app/javascript/views/pages/SplitPanes.vue
@@ -1,45 +1,47 @@
 <template>
   <splitpanes class="default-theme">
+    <pane
+      max-size="33"
+      min-size="10.45"
+      style="background-color:#3f0e40;"
+    ></pane>
 
-   <pane max-size="33" min-size="10.45" style="background-color:#3f0e40;" ></pane>
-
-   <pane max-size="100" min-size="67">
-<div class="editor">
-  <editor
-	api-key="v69g5pnxcpt0m2kmjntucft1sek0y2g7qw17n2scwu0pi76w"
-  :init="{
-      menubar: false,
-      statusbar:false,
-
-      plugins: 'lists link code',
-      toolbar: 'bold italic underline strikethrough | link |  bullist numlist  | alignleft | code',
-    }"
-  />
-</div>
-   </pane>
-
+    <pane max-size="100" min-size="67">
+      <div class="editor">
+        <editor
+          api-key="v69g5pnxcpt0m2kmjntucft1sek0y2g7qw17n2scwu0pi76w"
+          :init="{
+            menubar: false,
+            statusbar: false,
+            plugins: 'lists link code',
+            toolbar:
+              'bold italic underline strikethrough | link |  bullist numlist  | alignleft | code',
+          }"
+        />
+      </div>
+    </pane>
   </splitpanes>
- </template>
+</template>
 
- <script>
-
- import { Splitpanes, Pane } from 'splitpanes'
- import 'splitpanes/dist/splitpanes.css'
- import Editor from "@tinymce/tinymce-vue";
- export default {
-   components: {
+<script>
+import { Splitpanes, Pane } from 'splitpanes';
+import 'splitpanes/dist/splitpanes.css';
+import Editor from '@tinymce/tinymce-vue';
+export default {
+  components: {
     Splitpanes,
     Pane,
-    editor: Editor }
- }
- </script>
+    editor: Editor,
+  },
+};
+</script>
 
- <style>
- splitpanes{
- height: 100vh;
- }
+<style>
+splitpanes {
+  height: 100vh;
+}
 
- .editor {
+.editor {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -47,6 +49,4 @@
   color: #2c3e50;
   margin-top: 75vh;
 }
- </style>
-
-
+</style>
