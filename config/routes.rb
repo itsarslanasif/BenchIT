754aa087a6875e7a1ee996d9c4af8e4ef9628381
AlinaMustaqeem <alina.mustaqeem@devsinc.com>
2022-11-15 18:53:16 +0500

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
 
 
     root :to => "application#index"
+    # comment these line to get the default image
     match "*path", to: "application#index", format: false, via: :get
   end
 end
