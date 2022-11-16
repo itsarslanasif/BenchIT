a16b56a17a05a69bdef54a7115476823b299116a
MuhammadHammadShahzad <hammad.shahzada@devsinc.com>
2022-11-16 21:12:56 +0500

    3UFP5Z6 | Create Public Channels API | PR Suggestions Implemented

--- test/controllers/bench_channels_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class BenchChannelsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get bench_channels_create_url
-    assert_response :success
-  end
-end
