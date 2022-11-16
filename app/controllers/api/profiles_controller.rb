57cae9c64f3180b07b4e2ce3e576f0b6bc6da729
Muhammad Umar <umar.yasir@devsinc.com>
2022-11-16 17:11:56 +0500

    CU-3ufpcyv - Back-end
    - Added create action in Api::ProfilesController

--- app/controllers/api/profiles_controller.rb
+++ app/controllers/api/profiles_controller.rb
@@ -1,14 +1,27 @@
 class Api::ProfilesController < Api::ApiController
-  before_action :set_workspace, only: %i[index ]
+  before_action :set_workspace, only: %i[index create]
 
   def index
     @profiles = @workspace.profiles
+  end
+
+  def create
+    @profile = current_user.profiles.new(profile_params) # assuming that the user is already logged in
+    if @profile.save
+      render json: "Profile Added to #{@workspace.company_name}"
+    else
+      render json: @profile.errors
+    end
   end
 
   private
 
   def set_workspace
     @workspace = Workspace.find_by(id: params[:workspace_id])
+  end
+
+  def profile_params
+    params.require(:profile).permit(:workspace_id, :username, :description)
   end
 
 end
