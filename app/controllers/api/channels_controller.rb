e0892cb0a7d0b3a6e0f5c2ec0bae7ff4c537a51d
m-adnan-iqbal <107465032+m-adnan-iqbal@users.noreply.github.com>
2022-11-16 15:54:54 +0500

    3u4bw8n-worked on PR review comments

--- app/controllers/api/channels_controller.rb
+++ app/controllers/api/channels_controller.rb
@@ -2,16 +2,13 @@ class Api::ChannelsController < Api::ApiController
   before_action :set_channel, only: [:show]
   def index
     current_user = User.first
-    @participants = current_user.channel_participants.pluck(:bench_channel_id)
-    @channels = BenchChannel.where(id:@participants)
-    render json: @channels
+    render json: current_user.bench_channels
   end
 
   private
   def set_channel
     @channel = BenchChannel.find_by_id(params[:id])
     return if @channel.present?
-
     render json: { json: @channel.errors, status: :unprocessable_entity }
   end
 end
--- app/controllers/api/groups_controller.rb
+++ app/controllers/api/groups_controller.rb
@@ -2,16 +2,13 @@ class Api::GroupsController < Api::ApiController
   before_action :set_group, only: [:show]
   def index
     current_user = User.first
-    @participants = current_user.user_groups.pluck(:group_id)
-    @groups = Group.where(id:@participants)
-    render json: @groups
+    render json: current_user.user_groups
   end
 
   private
   def set_group
     @group = Group.find_by_id(params[:id])
     return if @group.present?
-
     render json: { json: @group.errors, status: :unprocessable_entity }
   end
 end
