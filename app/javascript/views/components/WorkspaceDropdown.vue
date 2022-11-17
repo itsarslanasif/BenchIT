9d9da105b4e6f11aaa0f023dcd1563390d72a788
ramsha <ramsha.syed@devsinc.com>
2022-11-17 22:55:18 +0500

    3u4dmfp|added workspace dropdown

---
+++ app/javascript/views/components/WorkspaceDropdown.vue
@@ -0,0 +1,28 @@
+<template>
+      <select>
+        <option >BenchIT</option>
+        <option >Terms and COnditons</option>
+        <option >Invite people to BenchIT</option>
+        <option >Create a channel</option>
+        <option >Preference</option>
+        <option >Administration</option>
+        <option >Tools</option>
+        <option >Sign in to BenchIT on mobile</option>
+        <option >Sign out of BenchIt</option>
+      </select>
+</template>
+<script>
+
+export default {
+  name: 'Workspacedropdown'
+
+}
+
+</script>
+<style>
+.workspacedropdown{
+
+}
+
+
+</style>
--- app/javascript/views/pages/SplitPanes.vue
+++ app/javascript/views/pages/SplitPanes.vue
@@ -4,7 +4,9 @@
       max-size="33"
       min-size="10.45"
       style="background-color: #3f0e40"
-    ></pane>
+    >
+    <WorkspaceDropdown />
+  </pane>
     <pane class="chatpane" max-size="100" min-size="67">
       <Chat />
     </pane>
@@ -14,13 +16,14 @@
 <script>
 import { Splitpanes, Pane } from 'splitpanes';
 import 'splitpanes/dist/splitpanes.css';
-
+import WorkspaceDropdown from '../components/WorkspaceDropdown.vue'
 import Chat from '../components/chatPane/Chat.vue';
 export default {
   components: {
     Splitpanes,
     Pane,
     Chat,
+    WorkspaceDropdown,
   },
 };
 </script>
