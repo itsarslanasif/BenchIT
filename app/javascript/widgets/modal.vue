cd02518235720cbc4fd71553cb47082d9a6b866f
Ali Aftab <110173898+ali-aftab-khokhar@users.noreply.github.com>
2022-11-18 16:16:11 +0500

    Update modal.vue

--- app/javascript/widgets/modal.vue
+++ app/javascript/widgets/modal.vue
@@ -16,19 +16,16 @@ export default {
 </script>
 
 <style scoped>
-
 .modalListStyle {
     border-radius: 4%;
     background-color: #18232bce;
     width: 30%;
 }
-
 .modalItem {
     padding: 20px;
     border-radius: 2%;
     transition-duration: 0.3s;
 }
-
 .modalItem:hover {
     background-color: #141d24e5;
     transition-duration: 0.3s;
