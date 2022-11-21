4b43fbe44606406290beb71b6ed0459807d1b6f1
MuhammadHammadShahzad <hammad.shahzada@devsinc.com>
2022-11-21 15:45:12 +0500

    Breaking Changes | Bug Fixed - Column renamed its usage should be updated. - Bug | current_workspace_id was misspelled.

--- app/controllers/api/v1/mentions_controller.rb
+++ app/controllers/api/v1/mentions_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::MentionsController < Api::ApiController
   def channels_list
-    @private_channels = BenchChannel.user_joined_private_channels(current_user.id, session[:cuurent_workspace_id])
-    @public_channels = BenchChannel.public_channels(session[:cuurent_workspace_id]).all
+    @private_channels = BenchChannel.user_joined_private_channels(current_user.id, session[:current_workspace_id])
+    @public_channels = BenchChannel.public_channels(session[:current_workspace_id]).all
   end
 
   def users_list
-    @users = User.workspace_users(session[:cuurent_workspace_id]).all
+    @users = User.workspace_users(session[:current_workspace_id]).all
   end
 end
