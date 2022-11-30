a7c04ff8ec88910ef75504046e44ee18e3f233bb
m-adnan-iqbal <107465032+m-adnan-iqbal@users.noreply.github.com>
2022-11-30 14:00:05 +0500

    CU-3r5ekvm - Create Code block in message feature
    # Conflicts:
    #	app/javascript/views/pages/Chat.vue

--- app/javascript/views/pages/Chat.vue
+++ app/javascript/views/pages/Chat.vue
@@ -8,27 +8,66 @@
     </div>
     <div class="h-3/4">
       <div class="m-4 pt-6">
-        <div v-if="showMentions || showChannels"
-          class="w-1/4 p-2 text-sm shadow-inner bg-secondary text-white absolute z-10">
-          <div v-if="(showMentions && hasMentionCommand) || (showChannels && hasChannelCommand)">
-            <div v-for="item in filteredList" :key="item.name" class="p-1 rounded-md hover:bg-secondaryHover"
-              @click="addMentionToText">
-              {{ item.name }}
+        <div
+          v-if="showMentions || showChannels"
+          class="w-1/4 p-2 text-sm shadow-inner bg-secondary text-white absolute z-10"
+        >
+          <div
+            v-if="showMentions && hasMentionCommand"
+            class="hover:bg-secondaryHover"
+          >
+            <div
+              v-for="user in allUsers"
+              :key="user.username"
+              class="p-1 rounded-md hover:bg-secondaryHover"
+              @click="addMentionToText"
+            >
+              {{ user.username }}
+            </div>
+          </div>
+          <div v-if="showChannels && hasChannelCommand">
+            <div
+              v-for="channel in allChannels"
+              :key="channel.name"
+              class="p-1 rounded-sm hover:bg-secondaryHover"
+              @click="addChannelToText"
+            >
+              {{ channel.name }}
             </div>
           </div>
         </div>
         <div class="relative">
-          <editor v-model="message" api-key="{{import.meta.env.VITE_EDITOR_API}}" :init="{
-            menubar: false,
-            statusbar: false,
-            plugins: 'lists link code',
-            toolbar:
-              'bold italic underline strikethrough | link |  bullist numlist  | alignleft | code',
-          }" />
+          <editor
+            v-model="message"
+            api-key="{{process.env.VITE_EDITOR_API}}"
+            :init="{
+              menubar: false,
+              statusbar: false,
+              plugins: 'lists link code',
+              toolbar:
+                'bold italic underline strikethrough | link |  bullist numlist  | alignleft | code',
+              codesample_languages: [none],
+              formats: {
+                code: {
+                  inline: 'false',
+                  selector: 'p',
+                  styles: {
+                    'background-color': '#D3D3D3',
+                    'border-radius': '3px',
+                    padding: '5px',
+                    'left-padding': '10px',
+                    margin: '0',
+                    'font-family': 'monospace',
+                  },
+                },
+              },
+            }"
+          />
         </div>
-        <button @click="sendMessage"
-          class="float-right px-6 py-2 bg-success m-3 rounded-md text-white hover:bg-successHover">
-          {{ $t('actions.send') }}
+        <button
+          class="float-right px-6 py-2 bg-success m-3 rounded-md text-white hover:bg-successHover"
+        >
+          {{ $t(CONSTANTS.SEND) }}
         </button>
       </div>
     </div>
