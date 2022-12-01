2cd3eb4e78351964bdbb7e88850f008e037185e3
MuhammadArshadHabib <arshad.habib@devsinc.com>
2022-12-01 16:42:10 +0500

    3qzh6u1 | DM with previos teammates back end implemented

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -50,6 +50,23 @@ class Api::V1::UsersController < Api::ApiController
     render json: message_data
   end
 
+  def previous_direct_messages
+    two_weaks_ago_time=Time.now.utc-86800*14
+    current_user=User.find(params[:id])
+    converation_ids = BenchConversation.where(conversationable_type: "User", sender_id: current_user).or(BenchConversation.where(conversationable_type: "User", conversationable_id: current_user)).pluck(:id)
+    if converation_ids.empty?
+      return render json: {message: "No DMs Found",status_code: 1, status: :unprocessable_entity }
+    else
+      bench_converations_ids=ConversationMessage.where(bench_conversation_id: converation_ids).where("created_at > ?", two_weaks_ago_time).distinct.pluck(:bench_conversation_id)
+      if bench_converations_ids.empty?
+        return render json: {message: "No DMs Found in 2 Weaks",status_code: 1, status: :unprocessable_entity }
+      else
+        dm_users_ids=BenchConversation.where(id: bench_converations_ids).pluck(:conversationable_id,:sender_id).flatten.uniq
+        render json: {users_ids: dm_users_ids,status_code: 2}
+      end
+    end
+  end
+
   private
 
   def set_user
