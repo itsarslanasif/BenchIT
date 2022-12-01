19890cfb4e39487f967830e6136bcd1116ef3957
Irfan Nazeer <irfan.nazeer@devsinc.com>
2022-12-01 21:17:53 +0500

    3qzhd16-PrivateChannel Modal-Frontend added
    # Conflicts:
    #	app/javascript/assets/constants.js
    #	app/javascript/views/components/channels/ChannelElement.vue
    #	app/javascript/views/components/channels/ChannelsAccordion.vue

---
+++ app/javascript/views/containers/ChannelDetailModal.vue
@@ -0,0 +1,49 @@
+<template>
+  <Transition name="modal-fade">
+    <div class="modal ml-20 mt-20 mb-20">
+      <header>
+        <button type="button" class="btn-close text-2xl" @click="detailsopen(false)">x
+        </button>
+        <div class="flex ml-10">
+          <i class="fas fa-hashtag mt-3 ml-1"></i>
+          <p class="font-black text-2xl ml-1"> {{ this.channelname }} </p>
+        </div>
+        <div class="flex ml-10">
+          <p class="ml-3 hover:font-black cursor-pointer">
+            {{ CONSTANTS.ABOUT }}
+          </p>
+          <p class="ml-3 hover:font-black cursor-pointer">
+            {{ CONSTANTS.MEMBERS }}
+          </p>
+          <p class="ml-3 hover:font-black cursor-pointer">
+            {{ CONSTANTS.INTEGRATIONS }}
+          </p>
+          <p class="ml-3 hover:font-black cursor-pointer">
+            {{ CONSTANTS.SETTINGS }}
+          </p>
+        </div>
+      </header>
+      <About :channelname = "this.channelname" />
+    </div>
+  </Transition>
+</template>
+
+<script>
+import About from '../components/channeldetail/About.vue';
+import { CONSTANTS } from '../../assets/constants.js';
+export default {
+  name: 'ChannelDetailModal',
+  components: { About },
+
+  data() {
+    return {
+      CONSTANTS: CONSTANTS,
+    }
+  },
+
+  props: {
+    channelname: String,
+    detailsopen: Function
+  }
+}
+</script>
