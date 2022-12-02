9c67316282e54b57b706a2427eb3ba63e9410ac3
MuhammadArshadHabib <arshad.habib@devsinc.com>
2022-12-02 14:31:52 +0500

    3qzh6u1 | Callback function implemented for checking dms

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < Api::ApiController
   before_action :set_user, only: %i[show]
+  before_action :set_previous_direct_messages, only: %i[previous_direct_messages]
 
   def index
     @users = User.all
@@ -18,25 +19,22 @@ class Api::V1::UsersController < Api::ApiController
   end
 
   def previous_direct_messages
-    two_weaks_ago_time=Time.now.utc-86800*14
-    current_user=User.find(params[:id])
-    converation_ids = BenchConversation.where(conversationable_type: "User", sender_id: current_user).or(BenchConversation.where(conversationable_type: "User", conversationable_id: current_user)).pluck(:id)
-    if converation_ids.empty?
-      return render json: {message: "No DMs Found",status_code: 1, status: :unprocessable_entity }
-    else
-      bench_converations_ids=ConversationMessage.where(bench_conversation_id: converation_ids).where("created_at > ?", two_weaks_ago_time).distinct.pluck(:bench_conversation_id)
-      if bench_converations_ids.empty?
-        return render json: {message: "No DMs Found in 2 Weaks",status_code: 1, status: :unprocessable_entity }
-      else
-        dm_users_ids=BenchConversation.where(id: bench_converations_ids).pluck(:conversationable_id,:sender_id).flatten.uniq
-        render json: {users_ids: dm_users_ids,status_code: 2}
-      end
-    end
+    dm_users_ids=BenchConversation.where(id: @bench_converations_ids).pluck(:conversationable_id,:sender_id).flatten.uniq
+    render json: {users_ids: dm_users_ids,status_code: 2}
   end
 
   private
 
   def set_user
     @receiver = User.find(params[:id])
+  end
+
+  def set_previous_direct_messages
+    current_user=User.first
+    two_weaks_ago_time=Time.now.utc-86800*14
+    conversation_ids = BenchConversation.where(conversationable_type: "User", sender_id: current_user).or(BenchConversation.where(conversationable_type: "User", conversationable_id: current_user)).pluck(:id)
+    return render json: {message: "No DMs Found",status_code: 1, status: :unprocessable_entity } if conversation_ids.empty?
+    @bench_converations_ids=ConversationMessage.where(bench_conversation_id: conversation_ids).where("created_at > ?", two_weaks_ago_time).distinct.pluck(:bench_conversation_id)
+    return render json: {message: "No DMs Found in 2 Weaks",status_code: 1, status: :unprocessable_entity } if @bench_converations_ids.empty?
   end
 end
