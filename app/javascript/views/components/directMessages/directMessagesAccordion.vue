0b35f0fa73c116f3662056a5ea255ce4247d68c2
Ali Aftab <110173898+ali-aftab-khokhar@users.noreply.github.com>
2022-12-06 14:03:06 +0500

    CU-3X5MGJH | Real-time-Sidebar

--- app/javascript/views/components/directMessages/directMessagesAccordion.vue
+++ app/javascript/views/components/directMessages/directMessagesAccordion.vue
@@ -3,26 +3,15 @@
     <AccordionList class="my-5 ml-3 text-base text-slate-50">
       <AccordionItem default-opened="true">
         <template #summary>{{ $t('direct_messages.direct_messages') }}</template>
-        <h5
-          v-for="userId in dmList"
-          :key="userId"
-          class="hover:bg-primaryHover"
-        >
-          <div
-            @click="goToChat(`/users/${userId}`)"
-            class="-ml-3 pl-3 flex items-start py-1 cursor-pointer hover:bg-primaryHover"
-          >
-            <img
-              class="w-5 h-5 rounded-md"
-              src="https://i.pinimg.com/736x/55/0f/49/550f49a459548599a5a4ea1c67fc0244.jpg"
-            />
-            <p class="ml-3 text-xs  text-white">{{ userId }}</p>
+        <h5 v-for="user in dmList" :key="user.id" class="hover:bg-primaryHover">
+          <div @click="goToChat(`/users/${user.id}`)"
+            class="-ml-3 pl-3 flex items-start py-1 cursor-pointer hover:bg-primaryHover">
+            <img class="w-5 h-5 rounded-md"
+              src="https://i.pinimg.com/736x/55/0f/49/550f49a459548599a5a4ea1c67fc0244.jpg" />
+            <p class="ml-3 text-xs  text-white">{{ user.name }}</p>
           </div>
         </h5>
-        <div
-          class="-ml-3 flex items-start py-1 hover:bg-primaryHover"
-          @click="modalOpen = !modalOpen"
-        >
+        <div class="-ml-3 flex items-start py-1 hover:bg-primaryHover" @click="modalOpen = !modalOpen">
           <addTeammatesDropdown :items="options"></addTeammatesDropdown>
         </div>
       </AccordionItem>
