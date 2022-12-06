0128f02f37ed1f8d921e4962ab97807fa88a6aab
MuhammadArshadHabib <arshad.habib@devsinc.com>
2022-12-06 15:33:38 +0500

    3w8wvj3 | Spacing issues resolved

--- app/controllers/api/v1/favourites_controller.rb
+++ app/controllers/api/v1/favourites_controller.rb
@@ -1,36 +1,36 @@
 class Api::V1::FavouritesController < Api::ApiController
 
   def create
-    already_favourite=Favourite.where(user_id: params[:user_id],favourable_type: params[:favourable_type],favourable_id: params[:favourable_id]).pluck(:id)
+    current_user = User.first
+    already_favourite = Favourite.where(user_id:current_user, favourable_type: params[:favourable_type], favourable_id: params[:favourable_id]).pluck(:id)
     if already_favourite.empty?
-      favourite=Favourite.new(user_id:params[:user_id],favourable_type: params[:favourable_type],favourable_id:params[:favourable_id]).save
+      favourite = Favourite.new(user:current_user, favourable_type: params[:favourable_type], favourable_id:params[:favourable_id]).save
       if favourite
-        return render status: 201, json: {message: 'Succesfully added to favourites' ,status_code: 2}
+        return render status: 201, json: { success: 'Succesfully added to favourites' }
       else
-        render json: {message: "Cannot Add to Favoruites",status_code: 1, status: :unprocessable_entity }
+        render json: { error: "Cannot Add to Favoruites", status: :unprocessable_entity }
       end
     else
-      render json: {message: "Already Favoruite",status_code: 1, status: :unprocessable_entity }
+      render json: { error: "Already Favoruite", status: :unprocessable_entity }
     end
   end
 
   def destroy
-    @favourite=Favourite.find_by_id(params[:id])
+    @favourite = Favourite.find_by_id(params[:id])
     if @favourite
       if @favourite.destroy
-        render status: 201, json: {message: 'Succesfully removed from favourites' ,status_code: 2}
+        render status: 201, json: { success: 'Succesfully removed from favourites' }
       else
-        render json: {message: "Cannot Remove from favourites",status_code: 1, status: :unprocessable_entity }
+        render json: { error: "Cannot Remove from favourites", status: :unprocessable_entity }
       end
     else
-      render json: {message: "Cannot found in favourites",status_code: 1, status: :unprocessable_entity }
+      render json: { error: "Cannot found in favourites", status: :unprocessable_entity }
     end
   end
 
   private
 
   def favourites_params
-    params.permit(:user_id,:favourable_type,:favourable_id)
+    params.permit(:favourable_type,:favourable_id)
   end
-
 end
