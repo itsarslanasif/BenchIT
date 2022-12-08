34197fe956b7c43d8d9b8b7e94d4da9445bac646
MuhammadArshadHabib <arshad.habib@devsinc.com>
2022-12-08 18:12:30 +0500

    3qzhd5p | Queries changes, response also changed

--- app/controllers/api/v1/channel_participants_controller.rb
+++ app/controllers/api/v1/channel_participants_controller.rb
@@ -2,31 +2,27 @@ class Api::V1::ChannelParticipantsController < Api::ApiController
   before_action :check_channel_participants, only: %i[create]
 
   def create
-    @channel_participant_users = ChannelParticipant.where(user_id:@users_list_provided,bench_channel_id: @channel_id).pluck(:id)
+    @channel_participant_users = ChannelParticipant.where(user_id: params[:user_ids], bench_channel_id: @channel_id).pluck(:id)
+    p "Arshad Arshad"
     if @channel_participant_users.empty?
-      @users_list_provided.map {|user|ChannelParticipant.new(bench_channel_id: @channel_id[0], user_id: user,permission: true).save}
-      users_emails_joined = User.where(id: @users_list_provided).pluck(:email)
-      return render status: 201, json: { new_member_added: users_emails_joined ,status_code: 2 }
+      params[:user_ids].map {|user|ChannelParticipant.new(bench_channel_id: @channel_id[0], user_id: user,permission: true).save}
+      @users_joined = User.where(id: params[:user_ids]).pluck(:name)
+      return render status: 201, json: { members: @users_joined }
     else
-      render json: { message: "One or Many Users already participant of this channel", status_code: 1, status: :unprocessable_entity }
+      render json: { error: "One or Many Users already participant of this channel", status: :unprocessable_entity }
     end
   end
 
   private
 
   def channel_participant_params
-    params.permit(:bench_channel_name,:user_emails)
+    params.required(:channel_participant).permit(:bench_channel_name, :user_ids)
   end
 
   def check_channel_participants
     @channel_id = BenchChannel.where(name: params[:bench_channel_name]).pluck(:id)
     if @channel_id.empty?
-      render json: { message: "channel not found", status_code: 1, status: :unprocessable_entity }
-    else
-      @users_list_provided = User.where(email: params[:user_emails]).pluck(:id)
-      if @users_list_provided.empty?
-        render json: { message: "No User Found or Wrong Emails Provided", status_code: 1, status: :unprocessable_entity }
-      end
+      render json: { error: "channel not found", status: :unprocessable_entity }
     end
   end
 end
