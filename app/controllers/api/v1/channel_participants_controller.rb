238974fd05fb3cd0a9a6b2e6d075a3a9fe182110
MuhammadArshadHabib <arshad.habib@devsinc.com>
2022-12-12 14:17:59 +0500

    3qzhd5p | get users query moved to before action

--- app/controllers/api/v1/channel_participants_controller.rb
+++ app/controllers/api/v1/channel_participants_controller.rb
@@ -2,8 +2,7 @@ class Api::V1::ChannelParticipantsController < Api::ApiController
   before_action :check_channel_participants, only: %i[create]
 
   def create
-    params[:user_ids].map { |user| ChannelParticipant.create(bench_channel_id: @channel_id.id, user_id: user, permission: true) }
-    @users_joined = User.where(id: params[:user_ids]).pluck(:name)
+    params[:user_ids].map { |user_id| ChannelParticipant.create(bench_channel_id: @channel_id.id, user_id: user_id, permission: true) }
     render status: :created, json: { members: @users_joined }
   end
 
@@ -15,8 +14,9 @@ class Api::V1::ChannelParticipantsController < Api::ApiController
 
   def check_channel_participants
     @channel_id = BenchChannel.find_by(name: params[:bench_channel_name])
-    render json: { error: 'channel not found', status: :unprocessable_entity } if @channel_id.nil?
+    render json: { error: 'Channel Not Found', status: :unprocessable_entity } if @channel_id.nil?
     @channel_participants = ChannelParticipant.where(user_id: params[:user_ids], bench_channel_id: @channel_id.id).ids
     render json: { error: 'One or Many Users already participant of this channel', status: :unprocessable_entity } if @channel_participants.empty?
+    @users_joined = User.where(id: params[:user_ids]).pluck(:name)
   end
 end
