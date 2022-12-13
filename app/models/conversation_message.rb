f56192ff8b73adcd0e8b5341cd19ac5d6f08f2cd
MuhammadHammadShahzad <hammad.shahzada@devsinc.com>
2022-12-13 16:13:25 +0500

    CU-3Y21FFP | File Upload - Saving Files with the message - Locally saving in development as well as on aws in production

--- app/models/conversation_message.rb
+++ app/models/conversation_message.rb
@@ -1,16 +1,15 @@
 class ConversationMessage < ApplicationRecord
+  after_commit :broadcast_message
+
   belongs_to :bench_conversation
   belongs_to :user, foreign_key: :sender_id, inverse_of: :conversation_messages
-  after_commit :broadcast_message
+  belongs_to :parent_message, class_name: 'ConversationMessage', optional: true
 
   has_many_attached :message_attachments, dependent: :purge_later
 
   has_many :replies, class_name: 'ConversationMessage',
                      foreign_key: :parent_message_id, dependent: :destroy
-
   has_many :reactions, dependent: :destroy
-
-  belongs_to :parent_message, class_name: 'ConversationMessage', optional: true
 
   validates :content, presence: true, length: { minimum: 1, maximum: 100 }
 
