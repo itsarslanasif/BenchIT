634c4698409971060a55c12c558d2c603158c45c
faisalshehzaddev <faisal.shehzad@devsinc.com>
2022-12-13 20:08:24 +0500

    CU-3y21hw2 | includes inserted before the order

--- app/controllers/api/v1/conversation_messages_controller.rb
+++ app/controllers/api/v1/conversation_messages_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::ConversationMessagesController < Api::ApiController
   before_action :set_message, only: %i[destroy]
 
   def send_message
-    @messages = current_user.conversation_messages.order(created_at: :desc).includes(:user)
+    @messages = current_user.conversation_messages.includes(:user).order(created_at: :desc)
   end
 
   def create
