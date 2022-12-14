b86044b51dc63469dddd97974743c696970f9f84
Asad170170 <asadullah.siddiqui@devsinc.com>
2022-12-14 19:40:44 +0500

    CU-3wmheak | View-Channel-Members
    -Ui fixes

--- app/javascript/views/components/channeldetail/members.vue
+++ app/javascript/views/components/channeldetail/members.vue
@@ -3,29 +3,38 @@
     <div class="py-5">
       <input
         class="searchbar shadow bg-neutral-900 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        type="text" v-model="query" placeholder="Find members" @keyup="searchQuery()" />
-    </div >
-    <div v-if="channelDetailStore.channelMembers.length > 0" class="maxHeight overflow-auto ">
-          <div v-for="member in channelDetailStore.channelMembers" :key="member.id">
-            <MermberCard
-              class="cursor-pointer"
-              :name="member.username"
-              :description="member.description"
-              :img-url="member.image_url"
-            />
-          </div>
+        type="text"
+        v-model="query"
+        :placeholder="$t('channel_details.find_members')"
+        @keyup="searchQuery()"
+      />
     </div>
-    <p v-if="channelDetailStore.channelMembers.length == 0"> No matches found for {{query}} </p>
+    <div
+      v-if="channelDetailStore.channelMembers.length > 0"
+      class="maxHeight overflow-auto"
+    >
+      <div v-for="member in channelDetailStore.channelMembers" :key="member.id">
+        <MermberCard
+          class="cursor-pointer"
+          :name="member.username"
+          :description="member.description"
+          :img-url="member.image_url"
+        />
+      </div>
+    </div>
+    <p v-if="channelDetailStore.channelMembers.length == 0">
+      {{ $t('channel_details.no_matches_found') }} " {{ query }} "
+    </p>
   </div>
 </template>
 
 <script>
-import MermberCard from '../directMessages/memberCard.vue'
+import MermberCard from '../directMessages/memberCard.vue';
 import { useChannelDetailStore } from '../../../stores/useChannelDetailStore.js';
 export default {
   name: 'About',
-  components:{MermberCard},
-  query:'',
+  components: { MermberCard },
+  query: '',
   mounted() {
     this.searchQuery();
   },
@@ -35,18 +44,14 @@ export default {
   },
   methods: {
     async searchQuery() {
-      let channel_id=window.location.pathname.split('/')[2]
-      await this.channelDetailStore.getChannelMembers(
-        this.query,
-        channel_id
-      );
+      let channel_id = window.location.pathname.split('/')[2];
+      await this.channelDetailStore.getChannelMembers(this.query, channel_id);
     },
-  }
-
-}
+  },
+};
 </script>
 <style scoped>
-.maxHeight{
+.maxHeight {
   max-height: 400px;
 }
 </style>
--- app/javascript/views/components/channels/ChannelMembersInfo.vue
+++ app/javascript/views/components/channels/ChannelMembersInfo.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="flex p-2 bg-slate-400 rounded-md border-2 border-black">
+  <div class="flex p-2 bg-slate-800 rounded-md border border-slate-300">
     <div @click="showMemberClickListener(true)" class="flex cursor-pointer">
       <img
-        class="w-6 h-6 -ml-1 rounded-md border-2 border-primary"
+        class="w-6 h-6 -ml-1 rounded-md border border-slate-300"
         src="https://i.pinimg.com/736x/55/0f/49/550f49a459548599a5a4ea1c67fc0244.jpg"
       />
       <img
-        class="w-6 h-6 -ml-1 rounded-md border-2 border-primary"
+        class="w-6 h-6 -ml-1 rounded-md border border-slate-300"
         src="https://i.pinimg.com/736x/55/0f/49/550f49a459548599a5a4ea1c67fc0244.jpg"
       />
       <img
-        class="w-6 h-6 -ml-1 rounded-md border-2 border-primary"
+        class="w-6 h-6 -ml-1 rounded-md border border-slate-300"
         src="https://i.pinimg.com/736x/55/0f/49/550f49a459548599a5a4ea1c67fc0244.jpg"
       />
       <p class="ml-1 font-bold text-white">{{ count }}</p>
     </div>
-    <div class="rounded-md ml-2 border-2 border-black"></div>
+    <div class="rounded-md ml-2 border-2 border-slate-300"></div>
     <div class="ml-3 cursor-pointer">
       <AddPeopleToChannel />
     </div>
@@ -52,10 +52,3 @@ export default {
   },
 };
 </script>
-<style scoped>
-.vl {
-  width: 10px;
-  background-color: black;
-  height: 500px;
-}
-</style>
