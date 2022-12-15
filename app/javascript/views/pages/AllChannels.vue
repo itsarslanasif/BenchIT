8cd5ce7ccf170b94a0e6e0ce9ec722fb49dcc391
AliatDevsinc <ali.zafar@devsinc.com>
2022-12-15 06:27:17 -0500

    3qzhd2q | Adjusted Front-end to reflect backend changes

--- app/javascript/views/pages/AllChannels.vue
+++ app/javascript/views/pages/AllChannels.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="screen-height">
     <div class="sticky w-full h-auto bg-blue-500 divide-y divide-slate-200">
-      <div class="px-5 py-3 text-xl font-bold">{{$t('channels.all_channels')}}</div>
+      <div class="px-5 py-3 text-xl font-bold">
+        {{ $t('channels.all_channels') }}
+      </div>
       <div class="px-5 py-3">
         <n-space vertical class="w-full">
           <form @submit.prevent="handleSubmit">
@@ -13,21 +15,40 @@
               :placeholder="$t('channels.search_by_name_or_desc')"
             />
           </form>
-          <p class="text-small text-gray-100 font-thin">{{ channels?.query?.length}} {{$t('channels.result')}}</p>
+          <p class="text-small text-gray-100 font-thin">
+            {{ channels?.length }} {{ $t('channels.result') }}
+          </p>
         </n-space>
       </div>
     </div>
     <div class="px-5 py-3">
       <div
-        class="hover:bg-slate-100 py-3 rounded-md flex "
+        class="hover:bg-slate-100 py-3 rounded-md flex"
         @mouseover="showButton = true"
         @mouseleave="showButton = false"
-        v-for="channel in channels.query"
+        v-for="channel in channels"
         :key="channel.id"
       >
-        <div class="w-5/6 px-2 py-3 font-bold	relative">#{{ channel.name }}</div>
-        <div class="absolute px-5 py-8 font-thin">{{channel.description}}</div>
-        <div class="py-3" @click="joinChannel(channel.id)" v-if="showButton"><n-button type="success">{{$t('actions.join')}}</n-button></div>
+        <div class="w-5/6 px-2 py-3 font-bold relative">
+          #{{ channel.name }}
+        </div>
+        <div class="absolute px-5 py-8 font-thin">
+          {{ channel.description }}
+        </div>
+        <div
+          class="py-3 px-1"
+          @click="joinChannel(channel.id)"
+          v-if="showButton"
+        >
+          <n-button>{{ $t('actions.view') }}</n-button>
+        </div>
+        <div
+          class="py-3 px-1"
+          @click="joinChannel(channel.id)"
+          v-if="showButton"
+        >
+          <n-button type="success">{{ $t('actions.join') }}</n-button>
+        </div>
       </div>
     </div>
   </div>
@@ -44,19 +65,20 @@ export default {
   components: {
     NInput,
     NSpace,
-    NButton
+    NButton,
   },
   data() {
     return {
       channels: [],
       term: '',
       currentUser: {},
-      showButton: false
+      showButton: false,
     };
   },
   setup() {
     const currentUserStore = useCurrentUserStore();
     const { currentUser } = storeToRefs(currentUserStore);
+
     return {
       currentUser,
     };
@@ -72,6 +94,9 @@ export default {
       );
       this.$router.push(`/channels/${channel_id}`);
     },
+  },
+  async mounted() {
+    this.channels = await getChannels();
   },
 };
 </script>
