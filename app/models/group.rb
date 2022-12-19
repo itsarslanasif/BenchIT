139e282ee48869a529b15552099802d09e613db6
faisalshehzaddev <faisal.shehzad@devsinc.com>
2022-12-19 12:45:56 +0500

    CU-866940x4d | group name and validations setup

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,12 @@
 class Group < ApplicationRecord
   has_one :bench_conversation, as: :conversationable, dependent: :destroy
+
+  validates :members, presence: true, length: { minimum: 2 }, uniqueness: true
+  validates_inclusion_of :members, in: Profile.ids
+
+  private
+
+  def name
+    Profile.where(id: members).pluck(:username).join(',')
+  end
 end
