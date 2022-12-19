8f01d437a34e3d4bbdcac2a718f575a9e54b6de5
faisalshehzaddev <faisal.shehzad@devsinc.com>
2022-12-19 15:33:42 +0500

    CU-866940x4d | group name accessibility error resolved

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,8 +4,6 @@ class Group < ApplicationRecord
   validates :members, presence: true, length: { minimum: 2 }
   validates :members, inclusion: { in: Profile.ids }
 
-  private
-
   def name
     Profile.where(id: members).pluck(:username).join(',')
   end
