2e067bfee804599b7786e187867f384c18a92fb9
faisalshehzaddev <faisal.shehzad@devsinc.com>
2022-12-20 12:57:03 +0500

    CU-866940x4d | mention and reaction controller updated with profile

--- app/controllers/api/v1/mentions_controller.rb
+++ app/controllers/api/v1/mentions_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MentionsController < Api::ApiController
   def channels_list
-    @private_channels = BenchChannel.user_joined_private_channels(current_user.id, session[:current_workspace_id])
+    @private_channels = BenchChannel.user_joined_private_channels(current_profile.id, session[:current_workspace_id])
     @public_channels = BenchChannel.public_channels(session[:current_workspace_id]).all
   end
 
--- app/controllers/api/v1/reactions_controller.rb
+++ app/controllers/api/v1/reactions_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ReactionsController < Api::ApiController
   before_action :set_reaction, only: :destroy
 
   def create
-    @reaction = current_user.reactions.new(reaction_params)
+    @reaction = current_profile.reactions.new(reaction_params)
 
     if @reaction.save
       render json: 'Reaction added.'
