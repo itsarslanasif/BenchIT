d3107cac22a8d40400596ad2c00a361f497aa8c1
Irfan Nazeer <irfan.nazeer@devsinc.com>
2022-12-20 18:50:12 +0500

    CU-86694wrj1 | Bug BookMark Implementation In DM Resolve Robocop Issues

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,9 +1,8 @@
 class Bookmark < ApplicationRecord
-  belongs_to :user, optional:true
-  belongs_to :bench_channel, optional:true
+  belongs_to :user, optional: true
+  belongs_to :bench_channel, optional: true
   belongs_to :profile
 
   validates :bookmark_URL, presence: true
   validates :name, length: { maximum: 50 }
-
 end
