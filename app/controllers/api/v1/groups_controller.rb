4c6d4753b550c5ffb641a6defdfa49f48de322aa
faisalshehzaddev <faisal.shehzad@devsinc.com>
2022-12-20 12:49:36 +0500

    CU-866940x4d | group controller updated with profile

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -2,17 +2,17 @@ class Api::V1::GroupsController < Api::ApiController
   before_action :set_group, only: %i[show]
 
   def index
-    render json: current_user.groups
+    render json: current_profile.groups
   end
 
   def show
-    @messages = @group.bench_conversation.conversation_messages.includes(:user, :reactions).with_attached_message_attachments
+    @messages = @group.bench_conversation.conversation_messages.includes(:profile, :reactions).with_attached_message_attachments
   end
 
   private
 
   def set_group
     @group = Group.find(params[:id])
-    render json: { message: 'User is not part of this group', status: :not_found } unless current_user.group_ids.include?(@group.id)
+    render json: { message: 'User is not part of this group', status: :not_found } unless current_profile.group_ids.include?(@group.id)
   end
 end
