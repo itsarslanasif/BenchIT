65ddf48bfdeead89c96caafcb6c6b4476c87b56e
faisalshehzaddev <faisal.shehzad@devsinc.com>
2022-12-22 19:01:51 +0500

    CU-866940w4d | comments removed

--- app/javascript/views/components/chats/ChatHeader.vue
+++ app/javascript/views/components/chats/ChatHeader.vue
@@ -80,8 +80,8 @@ export default {
         id: window.location.pathname.split('/')[2],
         name: msg[0]?.channel_name
           ? msg[0]?.channel_name
-          : msg[0]?.group_id
-          ? 'Group Chat ' + msg[0]?.group_id
+          : msg[0]?.group_name
+          ? 'Group Chat ' + msg[0]?.group_name
           : msg[0]?.receiver_name,
         isActive: true,
         status: '',
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,6 @@
 class Group < ApplicationRecord
   has_one :bench_conversation, as: :conversationable, dependent: :destroy
   has_one :favourite, as: :favourable, dependent: :destroy
-  #belongs_to :profile, foreign_key: :members, inverse_of: :conversation_messages
 
   scope :joined_groups, ->(id) { where("'#{id}' = ANY (members)") }
 
