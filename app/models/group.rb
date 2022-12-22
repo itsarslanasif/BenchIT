3d3b21d2c51bc1cb3ff32687a2a9c11050464509
faisalshehzaddev <faisal.shehzad@devsinc.com>
2022-12-22 19:36:08 +0500

    CU-866940w4d | rubocop offense removed

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
   has_one :bench_conversation, as: :conversationable, dependent: :destroy
   has_one :favourite, as: :favourable, dependent: :destroy
-  
+
   scope :joined_groups, ->(id) { where('? = ANY (members)', id) }
 
   def name
