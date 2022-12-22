4768687ddf568848664aa2f69ba478d4aed86b33
Ali Aftab <110173898+ali-aftab-khokhar@users.noreply.github.com>
2022-12-22 19:15:13 +0500

    CU-86695FEGN | Implement Jump Date Functionality

--- app/javascript/views/components/chats/ChatBody.vue
+++ app/javascript/views/components/chats/ChatBody.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="overflow-auto chatBody">
     <PinnedConversationModel />
-    <div v-for="message in messages" :key="message.id" :id="message.id">
+    <div v-for="message in messages" :key="message.id" :id="message.created_at">
       {{ setMessage(message) }}
       <div v-if="!isSameDayMessage && !message.parent_message_id">
-        <n-divider v-if="isToday" class="text-xs">
-          <p class="font-normal text-xs">{{$t('chat.today')}}</p>
+        <n-divider v-if="isToday" class="text-xs relative" @click="toggleToday" v-on:click.self="toggleToday">
+          <div>
+            <p class="font-normal text-xs border px-4 py-1 rounded-full">
+              {{ $t('chat.today') }}
+            </p>
+          </div>
+          <div v-if="jumpToDateTodayToggle" @close="toggleToday" class="absolute top-0 mt-8 w-1/5 z-10">
+            <JumpToDateVue />
+          </div>
         </n-divider>
-        <n-divider v-else class="text-xs text-black-500">
-          <p class="font-normal text-xs text-black-500">{{ new Date(message.created_at).toDateString() }}</p>
+        <n-divider v-else class="text-xs">
+          <p
+            class="font-normal text-xs border px-4 py-1 rounded-full"
+            @click="toggleNotToday"
+          >
+            {{ new Date(message.created_at).toDateString() }}
+          </p>
+          <div v-if="jumpToDateToggle" class="absolute top-0 mt-8 w-1/5 z-10">
+            <JumpToDateVue />
+          </div>
         </n-divider>
       </div>
       <MessageWrapper
@@ -26,7 +41,7 @@ import { useMessageStore } from '../../../stores/useMessagesStore';
 import { NButton, NSpace, NDivider } from 'naive-ui';
 import { storeToRefs } from 'pinia';
 import PinnedConversationModel from '../pinnedConversation/pinnedConversationModel.vue';
-
+import JumpToDateVue from '../../widgets/JumpToDate.vue';
 export default {
   name: 'ChatBody',
   components: {
@@ -35,11 +50,18 @@ export default {
     NButton,
     NSpace,
     PinnedConversationModel,
+    JumpToDateVue,
   },
   data() {
     return {
       messages: [],
+      jumpToDateTodayToggle: false,
+      jumpToDateToggle: false,
       prevMessage: null,
+      typeJump: {
+        today: false,
+        not_today: false,
+      },
     };
   },
   computed: {
@@ -61,18 +83,23 @@ export default {
     const { messages, currMessage } = storeToRefs(messageStore);
     return {
       messages,
-      currMessage
+      currMessage,
     };
   },
   methods: {
     setMessage(message) {
       this.prevMessage = this.currMessage;
-      this.currMessage = message
+      this.currMessage = message;
+    },
+    toggleToday() {
+      this.jumpToDateTodayToggle = !this.jumpToDateTodayToggle;
+    },
+    toggleNotToday() {
+      this.jumpToDateToggle = !this.jumpToDateToggle;
     },
   },
   updated() {
     const message_id = this.$route.params.message_id;
-
     if (message_id) {
       const message = document.getElementById(message_id);
       message.scrollIntoView();
---
+++ app/javascript/views/widgets/JumpToDate.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="z-10 w-full border text-white drop-shadow-2xl bg-primary py-3 rounded-md text-sm font-normal"
+  >
+    <p class="text-xs toDays py-1">
+      {{ $t('messages_section.jump_to') }}
+    </p>
+    <div class="toDays hover:bg-primaryHover">
+      {{ $t('messages_section.today') }}
+    </div>
+    <div class="toDays hover:bg-primaryHover">
+      {{ $t('messages_section.yesterday') }}
+    </div>
+    <div class="toDays hover:bg-primaryHover">
+      {{ $t('messages_section.last_week') }}
+    </div>
+    <div class="toDays hover:bg-primaryHover">
+      {{ $t('messages_section.last_month') }}
+    </div>
+    <div class="toDays hover:bg-primaryHover">
+      {{ $t('messages_section.the_very_beginning') }}
+    </div>
+    <hr class="border-primaryHover my-2" />
+    <div class="toDays hover:bg-primaryHover">
+      {{ $t('messages_section.jump_to_specific_date') }}
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+.toDays {
+  @apply px-5;
+}
+</style>
