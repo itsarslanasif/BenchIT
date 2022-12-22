ecc6c183b6268266be0f4563228093bb9789fc0f
faisalshehzaddev <faisal.shehzad@devsinc.com>
2022-12-22 20:03:51 +0500

    CU-866940w4d | group validation added

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,8 @@ class Group < ApplicationRecord
   has_one :bench_conversation, as: :conversationable, dependent: :destroy
   has_one :favourite, as: :favourable, dependent: :destroy
 
+  validates :members, inclusion: { in: Profile.ids }
+
   scope :joined_groups, ->(id) { where('? = ANY (members)', id) }
 
   def name
