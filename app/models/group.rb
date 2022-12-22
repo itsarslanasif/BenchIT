2d97244d4c33a4c022c71d2a448ea9caf258a6f8
faisalshehzaddev <faisal.shehzad@devsinc.com>
2022-12-22 19:33:28 +0500

    CU-866940w4d | sql injection removed

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,8 @@
 class Group < ApplicationRecord
   has_one :bench_conversation, as: :conversationable, dependent: :destroy
   has_one :favourite, as: :favourable, dependent: :destroy
-
-  scope :joined_groups, ->(id) { where("'#{id}' = ANY (members)") }
+  
+  scope :joined_groups, ->(id) { where('? = ANY (members)', id) }
 
   def name
     'group_name'
