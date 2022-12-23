84c6706c27350ef1630abf667175258974818589
Ali Aftab <110173898+ali-aftab-khokhar@users.noreply.github.com>
2022-12-23 20:54:05 +0500

    CU-86695FEGN | Implement Jump Date Functionality - Functionality: Completed

--- app/javascript/views/components/chats/ChatBody.vue
+++ app/javascript/views/components/chats/ChatBody.vue
@@ -1,28 +1,38 @@
 <template>
   <div class="overflow-auto chatBody">
     <PinnedConversationModel />
-    <div v-for="message in messages" :key="message.id" :id="message.created_at">
+    <div
+      v-for="message in messages"
+      :key="message.id"
+      :id="getDate(message.created_at)"
+    >
       {{ setMessage(message) }}
       <div v-if="!isSameDayMessage && !message.parent_message_id">
-        <n-divider v-if="isToday" class="text-xs relative" @click="toggleToday" v-on:click.self="toggleToday">
+        <n-divider v-if="isToday" class="text-xs relative" @click="toggleToday">
           <div>
-            <p class="font-normal text-xs border px-4 py-1 rounded-full">
+            <p class="date hover:bg-slate-50">
               {{ $t('chat.today') }}
             </p>
           </div>
-          <div v-if="jumpToDateTodayToggle" @close="toggleToday" class="absolute top-0 mt-8 w-1/5 z-10">
-            <JumpToDateVue />
+          <div
+            v-if="jumpToDateTodayToggle"
+            class="absolute top-0 mt-8 w-1/5 z-10"
+          >
+            <JumpToDateVue
+              :scrollToMessageByDate="scrollToMessageByDate"
+              :today="true"
+            />
           </div>
         </n-divider>
-        <n-divider v-else class="text-xs">
-          <p
-            class="font-normal text-xs border px-4 py-1 rounded-full"
-            @click="toggleNotToday"
-          >
+        <n-divider v-else class="text-xs relative">
+          <p class="date hover:bg-slate-50" @click="toggleNotToday(message)">
             {{ new Date(message.created_at).toDateString() }}
           </p>
-          <div v-if="jumpToDateToggle" class="absolute top-0 mt-8 w-1/5 z-10">
-            <JumpToDateVue />
+          <div
+            v-if="jumpToDateToggle && message.id === selectedMessage.id"
+            class="absolute top-0 mt-8 w-1/5 z-10"
+          >
+            <JumpToDateVue :scrollToMessageByDate="scrollToMessageByDate" />
           </div>
         </n-divider>
       </div>
@@ -42,6 +52,7 @@ import { NButton, NSpace, NDivider } from 'naive-ui';
 import { storeToRefs } from 'pinia';
 import PinnedConversationModel from '../pinnedConversation/pinnedConversationModel.vue';
 import JumpToDateVue from '../../widgets/JumpToDate.vue';
+import moment from 'moment';
 export default {
   name: 'ChatBody',
   components: {
@@ -58,10 +69,7 @@ export default {
       jumpToDateTodayToggle: false,
       jumpToDateToggle: false,
       prevMessage: null,
-      typeJump: {
-        today: false,
-        not_today: false,
-      },
+      selectedMessage: {},
     };
   },
   computed: {
@@ -93,9 +101,37 @@ export default {
     },
     toggleToday() {
       this.jumpToDateTodayToggle = !this.jumpToDateTodayToggle;
+      this.jumpToDateToggle = false;
     },
-    toggleNotToday() {
+    toggleNotToday(message) {
+      this.selectedMessage = message;
       this.jumpToDateToggle = !this.jumpToDateToggle;
+      this.jumpToDateTodayToggle = false;
+    },
+    scrollToMessageByDate(date) {
+      let message = {};
+      let id = '';
+      if (date === 'beginning_of_chat') {
+        const beginningDate = new Date(this.messages[0].created_at);
+        id = `${beginningDate.getFullYear()}-${beginningDate.getMonth()}-${beginningDate.getDate()}`;
+      } else {
+        id = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+      }
+      message = document.getElementById(id);
+      this.scrollToMessage(message);
+    },
+    scrollToMessage(message) {
+      if (message) {
+        message.scrollIntoView();
+        message.classList.add('highlight');
+      }
+      this.jumpToDateTodayToggle = false;
+      this.jumpToDateToggle = false;
+    },
+    getDate(created_at) {
+      const dateInUTC = moment(new Date(created_at));
+      const date = new Date(dateInUTC);
+      return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
     },
   },
   updated() {
@@ -122,5 +158,9 @@ export default {
   0% {
     background: rgba(253, 245, 221, 255);
   }
+}
+
+.date {
+  @apply font-normal text-xs border px-4 py-1 rounded-full;
 }
 </style>
--- app/javascript/views/widgets/JumpToDate.vue
+++ app/javascript/views/widgets/JumpToDate.vue
@@ -5,29 +5,103 @@
     <p class="text-xs toDays py-1">
       {{ $t('messages_section.jump_to') }}
     </p>
-    <div class="toDays hover:bg-primaryHover">
+    <div
+      v-if="!this.today"
+      class="toDays hover:bg-primaryHover"
+      @click="setOption(1)"
+    >
       {{ $t('messages_section.today') }}
     </div>
-    <div class="toDays hover:bg-primaryHover">
+    <div class="toDays hover:bg-primaryHover" @click="setOption(2)">
       {{ $t('messages_section.yesterday') }}
     </div>
-    <div class="toDays hover:bg-primaryHover">
+    <div class="toDays hover:bg-primaryHover" @click="setOption(3)">
       {{ $t('messages_section.last_week') }}
     </div>
-    <div class="toDays hover:bg-primaryHover">
+    <div class="toDays hover:bg-primaryHover" @click="setOption(4)">
       {{ $t('messages_section.last_month') }}
     </div>
-    <div class="toDays hover:bg-primaryHover">
+    <div class="toDays hover:bg-primaryHover" @click="setOption(5)">
       {{ $t('messages_section.the_very_beginning') }}
     </div>
     <hr class="border-primaryHover my-2" />
-    <div class="toDays hover:bg-primaryHover">
+    <div class="toDays hover:bg-primaryHover" @click="setOption(6)">
       {{ $t('messages_section.jump_to_specific_date') }}
     </div>
   </div>
+  <n-modal v-model:show="openCalenderModal">
+    <n-card
+      style="width: 600px"
+      :title="$t('messages_section.jump_to_specific_date')"
+      :bordered="false"
+      size="huge"
+      role="dialog"
+      aria-modal="true"
+    >
+      <n-date-picker v-model:value="timestamp" />
+      <n-button class="mt-2 float-right px-10" @click="goToDate">
+        {{ $t('actions.go') }}
+      </n-button>
+    </n-card>
+  </n-modal>
 </template>
 <script>
-export default {};
+import { NModal, NCard, NDatePicker, NButton } from 'naive-ui';
+export default {
+  components: { NModal, NCard, NDatePicker, NButton },
+  props: ['scrollToMessageByDate', 'today'],
+  data() {
+    return {
+      openCalenderModal: false,
+      timestamp: null,
+    };
+  },
+  methods: {
+    selectedDate(date) {
+      console.log(date);
+    },
+    setOption(type) {
+      const date = new Date();
+      switch (type) {
+        case 1:
+          this.scrollToMessageByDate(date);
+          break;
+        case 2:
+          const yesterday = new Date(
+            date.getFullYear(),
+            date.getMonth(),
+            date.getDate() - 1
+          );
+          this.scrollToMessageByDate(yesterday);
+          break;
+        case 3:
+          const last_week = new Date(
+            date.getFullYear(),
+            date.getMonth(),
+            date.getDate() - 7
+          );
+          this.scrollToMessageByDate(last_week);
+          break;
+        case 4:
+          const last_month = new Date(date.getFullYear(), date.getMonth() - 1);
+          this.scrollToMessageByDate(last_month);
+          break;
+        case 5:
+          this.scrollToMessageByDate('beginning_of_chat');
+          break;
+        case 6:
+          this.openCalenderModal = !this.openCalenderModal;
+          break;
+        default:
+          break;
+      }
+    },
+    goToDate() {
+      const date = new Date(this.timestamp);
+      this.scrollToMessageByDate(date);
+    },
+  },
+};
 </script>
 <style>
 .toDays {
