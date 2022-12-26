c69d2ddfa8fb20dc1e4adbaa48f26016eb1454ba
Irfan Nazeer <irfan.nazeer@devsinc.com>
2022-12-26 15:53:10 +0500

    CU-86694wrj1 | Bug BookMark Implementation In DM

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,14 +1,10 @@
 class Bookmark < ApplicationRecord
-  belongs_to :user
-  belongs_to :bench_channel
-  before_validation :set_user
+  belongs_to :bookmarkable, polymorphic: true
 
   validates :bookmark_URL, presence: true
   validates :name, length: { maximum: 50 }
 
-  private
-
-  def set_user
-    self.user_id = Current.user.id
-  end
+  scope :with_bookmarkid_and_bookmarktype, lambda { |bookmark_id, bookmark_type|
+    where(bookmarkable_id: bookmark_id).where(bookmarkable_type: bookmark_type)
+  }
 end
