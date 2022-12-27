16539c92e3f8fc5685a7264d63a2ab42d58ce398
m-adnan-iqbal <107465032+m-adnan-iqbal@users.noreply.github.com>
2022-12-27 19:20:08 +0500

    updated base url

--- app/javascript/modules/axios/index.js
+++ app/javascript/modules/axios/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 export default axios.create({
-  baseURL: `${import.meta.env.VITE_APP_SERVER_URL}/api`,
+  baseURL: `http://localhost:3000/api`,
   headers: { Authorization: sessionStorage.getItem('token') },
 });
