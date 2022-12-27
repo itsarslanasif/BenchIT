aa411de7ef92b8e5ecff20d1e6f446eb88417c43
faisalshehzaddev <faisal.shehzad@devsinc.com>
2022-12-27 18:39:54 +0500

    CU-3wu468n | model level validations added

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,9 @@ class Group < ApplicationRecord
   has_one :bench_conversation, as: :conversationable, dependent: :destroy
   has_one :favourite, as: :favourable, dependent: :destroy
 
+  validates :profile_ids, presence: true, length: { in: 2..9 }
+  validates :profile_ids, inclusion: { in: Current.workspace.profile_ids }
+
   def name
     'group_name'
   end
