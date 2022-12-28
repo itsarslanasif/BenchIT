4c94396f26fc68ca2b2641a96239121ba7a100a1
MuhammadArshadHabib <arshad.habib@devsinc.com>
2022-12-28 19:42:33 +0500

    CU-86695mfe0 | Switch_workspace get API added, seed data added

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::ProfilesController < Api::ApiController
   before_action :check_profile_already_exists, only: %i[create]
   before_action :set_previous_direct_messages, only: %i[previous_direct_messages]
   before_action :check_user_member_of_workspace, only: %i[show update]
-  before_action :set_profile, only: %i[show update]
+  before_action :find_profile, only: %i[show update]
 
   def index
     @profiles = if params[:query].presence
@@ -47,7 +47,7 @@ class Api::V1::ProfilesController < Api::ApiController
 
   private
 
-  def set_profile
+  def find_profile
     @profile = Profile.find(params[:id])
   end
 
@@ -89,10 +89,10 @@ class Api::V1::ProfilesController < Api::ApiController
 
   def set_previous_direct_messages
     conversation_ids = BenchConversation.recent_last_conversation
-    return render json: [Current.profile] if conversation_ids.empty?
+    return render json: { profiles: [Current.profile] } if conversation_ids.empty?
 
     @bench_conversations_ids = ConversationMessage.recent_last_conversation(conversation_ids)
-    return render json: [Current.profile] if @bench_conversations_ids.empty?
+    return render json: { profiles: [Current.profile] } if @bench_conversations_ids.empty?
 
     @dm_users_ids = BenchConversation.where(id: @bench_conversations_ids).pluck(:conversationable_id, :sender_id).flatten.uniq
   end
