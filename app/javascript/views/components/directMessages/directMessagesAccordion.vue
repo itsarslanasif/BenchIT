b3a1c9cc2a1bac92f060293aa9e030adc7911a65
Ali Aftab <110173898+ali-aftab-khokhar@users.noreply.github.com>
2022-12-31 02:51:03 +0500

    CU-86696TV9P | DM List Bug - Bug fixed

--- app/javascript/views/components/directMessages/directMessagesAccordion.vue
+++ app/javascript/views/components/directMessages/directMessagesAccordion.vue
@@ -4,16 +4,22 @@
       <AccordionItem default-opened="true">
         <template #summary>
           {{ $t('direct_messages.direct_messages') }}
-          <a @click="handleClick"
-            class="px-2 hover-target py-1 h-5 ml-10 text-xs cursor-pointer text-center text-white rounded-md hover:bg-slate-800 border-slate-200 border border-solid">
+          <a
+            @click="handleClick"
+            class="px-2 hover-target py-1 h-5 ml-10 text-xs cursor-pointer text-center text-white rounded-md hover:bg-slate-800 border-slate-200 border border-solid"
+          >
             +
           </a>
         </template>
         <h5 v-for="user in dmList" :key="user.id" class="hover:bg-primaryHover">
-          <div @click="goToChat(`/profiles/${user.id}`)"
-            class="flex items-center -ml-3 pl-3 py-1 cursor-pointer hover:bg-primaryHover">
-            <img class="w-6 h-6 rounded-md"
-              src="https://i.pinimg.com/736x/55/0f/49/550f49a459548599a5a4ea1c67fc0244.jpg" />
+          <div
+            @click="goToChat(`/profiles/${user.id}`)"
+            class="flex items-center -ml-3 pl-3 py-1 cursor-pointer hover:bg-primaryHover"
+          >
+            <img
+              class="w-6 h-6 rounded-md"
+              src="https://i.pinimg.com/736x/55/0f/49/550f49a459548599a5a4ea1c67fc0244.jpg"
+            />
             <p class="ml-2 text-sm text-white">{{ user.username }}</p>
           </div>
         </h5>
@@ -57,15 +63,11 @@ export default {
     this.dmList = this.options = null;
   },
   mounted() {
-    this.directMessageStore.getDmList(
-      this.currentProfileStore.currentProfile.workspace_id
-    );
+    this.getDmList();
   },
   setup() {
-    const directMessageStore = useDirectMessagesStore();
     const selectScreen = useSelectedScreenStore();
-    const currentProfileStore = useCurrentProfileStore();
-    return { directMessageStore, currentProfileStore, selectScreen };
+    return { selectScreen };
   },
   methods: {
     closeModal() {
@@ -85,7 +87,7 @@ export default {
       this.$router.push(chatURL);
     },
     handleClick() {
-      this.$router.push('/new_direct_message')
+      this.$router.push('/new_direct_message');
     },
   },
 };
