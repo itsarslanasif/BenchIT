ebf1d68aadb3b8ffd9d4f10e59f1d20bb79af2f4
Ali Aftab <110173898+ali-aftab-khokhar@users.noreply.github.com>
2022-12-30 19:50:32 +0500

    CU-86696BN49 | Memory Leaks - Cleaning memory on unmount - Fixed Conditional Rendering

--- app/javascript/views/components/directMessages/findDirectMessages.vue
+++ app/javascript/views/components/directMessages/findDirectMessages.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class=" bg-secondary text-white border border-slate-600 h-screen -mb-12">
+  <div class="bg-secondary text-white border border-slate-600 h-screen -mb-12">
     <div class="grid text-slate-600 grid-cols-1 divide-y">
       <div class="p-2 text-white text-xl">
         {{ $t('direct_messages.direct_messages') }}
       </div>
-      <div >
+      <div>
         <div class="flex p-3">
           <p>{{ $t('request.to') }}:</p>
           <input
@@ -52,10 +52,6 @@ export default {
   },
   beforeUnmount() {
     this.query = this.members = this.users = null;
-  },
-  setup() {
-    const selectedDm = useDirectMessagesStore();
-    return { selectedDm };
   },
   methods: {
     async searchQuery() {
