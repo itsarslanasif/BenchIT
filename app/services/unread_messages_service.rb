3f181ebda7d967cd567676c0df986aa0039d1d9f
MuhammadHammadShahzad <hammad.shahzada@devsinc.com>
2022-12-30 12:42:17 +0500

    CU-3WU4280 Notifications - Rubocop Offences

--- app/services/unread_messages_service.rb
+++ app/services/unread_messages_service.rb
@@ -5,6 +5,12 @@ class UnreadMessagesService
     @message_id = message_id
   end
 
+  def call
+    add_in_redis
+  end
+
+  private
+
   def add_in_redis
     previous_unread_messages_details = REDIS.get("unreadMessages#{Current.workspace.id}#{@profile_id}")
     previous_unread_messages_details = previous_unread_messages_details.nil? ? [] : JSON.parse(previous_unread_messages_details)
@@ -13,9 +19,9 @@ class UnreadMessagesService
                                             conversationable_id: @bench_conversation.conversationable_id,
                                             message_id: @message_id
                                           })
-    unless @profile_id == Current.profile.id
-      REDIS.set("unreadMessages#{Current.workspace.id}#{@profile_id}",
-                previous_unread_messages_details.to_json)
-    end
+    return if @profile_id == Current.profile.id
+
+    REDIS.set("unreadMessages#{Current.workspace.id}#{@profile_id}",
+              previous_unread_messages_details.to_json)
   end
 end
