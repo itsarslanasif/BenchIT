4b9df1238f0b2041dfaef1700560faf4f66423a7
faisalshehzaddev <faisal.shehzad@devsinc.com>
2023-01-02 20:41:24 +0500

    CU-86696bn2c | reaction cable methods improve

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,37 +1,44 @@
 class Reaction < ApplicationRecord
   include Cablable
-  after_destroy :reaction_delete_broadcast
-  after_save :reaction_broadcast
+  after_create :set_action_create
+  after_update :set_action_update
+  after_destroy :set_action_delete
+  after_commit :reaction_broadcast
 
   belongs_to :profile
   belongs_to :conversation_message
 
   def reaction_broadcast
-    result = append_conversation_type_and_id(conversation_message.bench_conversation, set_reaction)
-    BroadcastMessageService.new(result, conversation_message.bench_conversation).call
-  end
-
-  def reaction_delete_broadcast
-    result = set_reaction
-    result[:action] = 'Delete'
+    reaction = set_reaction
+    result = {
+      content: reaction,
+      action: @action,
+      type: 'Reaction'
+    }
     result = append_conversation_type_and_id(conversation_message.bench_conversation, result)
     BroadcastMessageService.new(result, conversation_message.bench_conversation).call
   end
 
   private
 
+  def set_action_delete
+    @action = 'Delete'
+  end
+
+  def set_action_create
+    @action = 'Create'
+  end
+
+  def set_action_update
+    @action = 'Update'
+  end
+
   def set_reaction
-    reaction = {
+    {
       id: id,
       emoji: emoji,
       conversation_message_id: conversation_message_id,
       bench_conversation_id: conversation_message.bench_conversation_id
     }
-    result = {
-      content: reaction,
-      type: 'Reaction'
-    }
-    result[:action] = created_at == updated_at ? 'Create' : 'Update'
-    result
   end
 end
