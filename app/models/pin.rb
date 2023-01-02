f42031b79c65d1f6baaa4bd30ec92c346d9a9fef
faisalshehzaddev <faisal.shehzad@devsinc.com>
2023-01-02 21:01:43 +0500

    CU-86696bn2c | pin cable method improve

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,39 +1,45 @@
 class Pin < ApplicationRecord
   include Cablable
-  after_destroy :broadcast_unpin
-  after_save :broadcast_pin
+  after_create :set_action_create
+  after_update :set_action_update
+  after_destroy :set_action_delete
+  after_commit :broadcast_pin
 
   belongs_to :conversation_message
   belongs_to :profile
   belongs_to :bench_conversation
 
   def broadcast_pin
-    set_pin
-    @result[:action] = 'Pin'
-    @result = append_conversation_type_and_id(bench_conversation, @result)
-    BroadcastMessageService.new(@result, bench_conversation).call
-  end
-
-  def broadcast_unpin
-    set_pin
-    @result[:action] = 'Unpin'
-    @result = append_conversation_type_and_id(bench_conversation, @result)
-    BroadcastMessageService.new(@result, bench_conversation).call
+    pin = set_pin
+    result = {
+      content: pin,
+      action: @action,
+      type: 'Pin'
+    }
+    result = append_conversation_type_and_id(bench_conversation, result)
+    BroadcastMessageService.new(result, bench_conversation).call
   end
 
   private
 
+  def set_action_delete
+    @action = 'Unpin'
+  end
+
+  def set_action_create
+    @action = 'Pin'
+  end
+
+  def set_action_update
+    @action = 'Update'
+  end
+
   def set_pin
-    pin = {
+    {
       id: id,
       conversation_message_id: conversation_message_id,
       pin_by: profile.username,
       count: bench_conversation.pins.size
     }
-    @result = {
-      content: pin,
-      type: 'Pin'
-    }
-    @result[:action] = created_at == updated_at ? 'Create' : 'Update'
   end
 end
