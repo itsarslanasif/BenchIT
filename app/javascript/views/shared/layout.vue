2599a141eb6842d3d52e17ead7c889da3e27d63e
Ali Aftab <110173898+ali-aftab-khokhar@users.noreply.github.com>
2023-01-03 14:39:45 +0500

    CU-86696BNWW | Naive UI: Custom Theme

--- app/javascript/views/shared/layout.vue
+++ app/javascript/views/shared/layout.vue
@@ -1,17 +1,31 @@
 <template>
   <div class="max-h-full">
-    <router-view class="relative"/>
+    <n-config-provider :theme-overrides="themeOverrides">
+      <router-view class="relative" />
+    </n-config-provider>
   </div>
 </template>
 
 <script>
 import NavBar from './_nav.vue';
 import searchBarVue from './searchBar.vue';
+import { NConfigProvider } from 'naive-ui';
 
 export default {
   components: {
+    NConfigProvider,
     'nav-bar': NavBar,
-    'search-bar': searchBarVue
+    'search-bar': searchBarVue,
   },
-}
+  data() {
+    return {
+      themeOverrides: {
+        common: {
+          primaryColor: '#1E293B',
+          primaryColorHover: '#475569',
+        },
+      },
+    };
+  },
+};
 </script>
