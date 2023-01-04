7ea669a9eb23912327f3845038c42a6c029783bd
asadtariq-dev <asad.tariq@devsinc.com>
2023-01-04 13:22:57 +0500

    CU-3y7vzhw - Edit Contact Information - Slack Profile -FE
    * Resolved formatting issues

--- app/javascript/views/components/rightPane/UserProfile.vue
+++ app/javascript/views/components/rightPane/UserProfile.vue
@@ -2,98 +2,263 @@
   <right-pane-header :paneTitle="$t('rightpane.profile')" />
   <div class="flex flex-col overflow-auto paneBody">
     <div class="flex items-center justify-center mt-2">
-      <img :src="userProfileStore.user_profile?.image_url" alt="user avatar" class="rounded-lg w-90 h-90" />
+      <img
+        :src="userProfileStore.user_profile?.image_url"
+        alt="user avatar"
+        class="rounded-lg w-90 h-90"
+      />
     </div>
     <div class="flex flex-col mt-2">
       <span class="grid grid-cols-12 mx-4">
-        <p class="col-span-11 font-bold text-xl break-words">{{userProfileStore.user_profile?.username}}</p>
-        <p v-if="ownProfile" class="col-span-1 self-center text-info cursor-pointer hover:underline">{{$t('actions.edit')}}</p>
+        <p class="col-span-11 font-bold text-xl break-words">
+          {{ userProfileStore.user_profile?.username }}
+        </p>
+        <p
+          v-if="ownProfile"
+          class="col-span-1 self-center text-info cursor-pointer hover:underline"
+        >
+          {{ $t('actions.edit') }}
+        </p>
       </span>
-      <p v-if="userProfileStore.user_profile?.description" class="float-left text-md ml-4 break-words text-black-800 hover:underline cursor-pointer">{{userProfileStore.user_profile?.description}}</p>
-      <p v-if="ownProfile && !userProfileStore.user_profile?.description" class="text-sm ml-4 text-info cursor-pointer hover:underline">{{$t('rightpane.add_description')}}</p>
-      <p v-if="userProfileStore.user_profile?.pronounce_name" class="float-left text-md ml-4 break-words text-black-800 hover:underline cursor-pointer">{{userProfileStore.user_profile?.pronounce_name}}</p>
-      <p v-if="ownProfile && !userProfileStore.user_profile?.pronounce_name" class="text-sm ml-4 text-info cursor-pointer hover:underline">{{$t('rightpane.add_name_pronounciation')}}</p>
-      <span v-if="userProfileStore.user_profile?.status" class="flex float-left text-md mx-4 mt-2 break-words text-black-800">
-        <font-awesome-icon v-if="userProfileStore.user_profile?.isActive" icon="fa-circle" class="self-center text-success text-sm mr-2 px-1" />
-        <font-awesome-icon v-else icon="fa-circle" class="self-center text-sm mr-2 px-1" />
-        <p>{{userProfileStore.user_profile?.isActive ? $t('rightpane.active') : $t('rightpane.away') }}</p>
+      <p
+        v-if="userProfileStore.user_profile?.description"
+        class="float-left text-md ml-4 break-words text-black-800 hover:underline cursor-pointer"
+      >
+        {{ userProfileStore.user_profile?.description }}
+      </p>
+      <p
+        v-if="ownProfile && !userProfileStore.user_profile?.description"
+        class="text-sm ml-4 text-info cursor-pointer hover:underline"
+      >
+        {{ $t('rightpane.add_description') }}
+      </p>
+      <p
+        v-if="userProfileStore.user_profile?.pronounce_name"
+        class="float-left text-md ml-4 break-words text-black-800 hover:underline cursor-pointer"
+      >
+        {{ userProfileStore.user_profile?.pronounce_name }}
+      </p>
+      <p
+        v-if="ownProfile && !userProfileStore.user_profile?.pronounce_name"
+        class="text-sm ml-4 text-info cursor-pointer hover:underline"
+      >
+        {{ $t('rightpane.add_name_pronounciation') }}
+      </p>
+      <span
+        v-if="userProfileStore.user_profile?.status"
+        class="flex float-left text-md mx-4 mt-2 break-words text-black-800"
+      >
+        <font-awesome-icon
+          v-if="userProfileStore.user_profile?.isActive"
+          icon="fa-circle"
+          class="self-center text-success text-sm mr-2 px-1"
+        />
+        <font-awesome-icon
+          v-else
+          icon="fa-circle"
+          class="self-center text-sm mr-2 px-1"
+        />
+        <p>
+          {{
+            userProfileStore.user_profile?.isActive
+              ? $t('rightpane.active')
+              : $t('rightpane.away')
+          }}
+        </p>
       </span>
-      <span v-if="userProfileStore.user_profile?.status" class="flex float-left text-md mx-4 break-words text-black-800">
-        <p class="mr-3 text-lg">{{userProfileStore.user_profile?.status.emoji}}</p>
-        <p class="self-center">{{userProfileStore.user_profile?.status.text}}</p>
+      <span
+        v-if="userProfileStore.user_profile?.status"
+        class="flex float-left text-md mx-4 break-words text-black-800"
+      >
+        <p class="mr-3 text-lg">
+          {{ userProfileStore.user_profile?.status.emoji }}
+        </p>
+        <p class="self-center">
+          {{ userProfileStore.user_profile?.status.text }}
+        </p>
       </span>
       <span class="flex float-left mx-4 text-md break-words text-black-800">
         <font-awesome-icon icon="fa-clock" class="self-center text-lg mr-3" />
-        <p class="float-left text-md break-words text-black-800">{{userProfileStore.user_profile?.local_time}} {{$t('rightpane.local_time')}}</p>
+        <p class="float-left text-md break-words text-black-800">
+          {{ userProfileStore.user_profile?.local_time }}
+          {{ $t('rightpane.local_time') }}
+        </p>
       </span>
       <span class="flex float-left mt-2">
-        <button class="flex border border-black-800 rounded h-10 px-10 ml-4 hover:bg-transparent focus:outline-none">
-          <font-awesome-icon v-if="!ownProfile" icon="fa-comment-dots" class="self-center text-lg pr-1" />
-          <p class="self-center text-md">{{ownProfile ? $t('actions.set_a_status') : $t('actions.message')}}</p>
+        <button
+          class="flex border border-black-800 rounded h-10 px-10 ml-4 hover:bg-transparent focus:outline-none"
+        >
+          <font-awesome-icon
+            v-if="!ownProfile"
+            icon="fa-comment-dots"
+            class="self-center text-lg pr-1"
+          />
+          <p class="self-center text-md">
+            {{
+              ownProfile ? $t('actions.set_a_status') : $t('actions.message')
+            }}
+          </p>
         </button>
-        <button class="flex border border-black-800 rounded h-10 ml-2 hover:bg-transparent focus:outline-none">
-          <font-awesome-icon v-if="!ownProfile" icon="fa-headphones" class="self-center text-lg pr-1 pl-8" />
-          <p class="pr-6 self-center text-md" :class="{'pl-8' : ownProfile}">{{ ownProfile ? $t('actions.view_as') : $t('actions.huddle') }}</p>
-          <font-awesome-icon icon="fa-chevron-down" class="self-center text-xs pr-2" />
+        <button
+          class="flex border border-black-800 rounded h-10 ml-2 hover:bg-transparent focus:outline-none"
+        >
+          <font-awesome-icon
+            v-if="!ownProfile"
+            icon="fa-headphones"
+            class="self-center text-lg pr-1 pl-8"
+          />
+          <p class="pr-6 self-center text-md" :class="{ 'pl-8': ownProfile }">
+            {{ ownProfile ? $t('actions.view_as') : $t('actions.huddle') }}
+          </p>
+          <font-awesome-icon
+            icon="fa-chevron-down"
+            class="self-center text-xs pr-2"
+          />
         </button>
-        <button class="flex border border-black-800 rounded h-10 px-4 ml-2 hover:bg-transparent focus:outline-none">
-          <font-awesome-icon icon="fa-ellipsis-vertical" class="self-center text-lg" />
+        <button
+          class="flex border border-black-800 rounded h-10 px-4 ml-2 hover:bg-transparent focus:outline-none"
+        >
+          <font-awesome-icon
+            icon="fa-ellipsis-vertical"
+            class="self-center text-lg"
+          />
         </button>
       </span>
     </div>
     <n-divider v-if="userProfileStore.user_profile?.contact_info" />
-    <span v-if="userProfileStore.user_profile?.contact_info" class="flex flex-col float-left mx-4 mb-2 text-black-800">
+    <span
+      v-if="userProfileStore.user_profile?.contact_info"
+      class="flex flex-col float-left mx-4 mb-2 text-black-800"
+    >
       <span class="grid grid-cols-12">
-        <p class="col-span-11 font-bold text-md break-words">{{$t('rightpane.contact_information')}}</p>
-        <p v-if="ownProfile" @click="toggleEditContactInfoModal" class="col-span-1 self-center text-info cursor-pointer hover:underline">{{$t('actions.edit')}}</p>
+        <p class="col-span-11 font-bold text-md break-words">
+          {{ $t('rightpane.contact_information') }}
+        </p>
+        <p
+          v-if="ownProfile"
+          @click="toggleEditContactInfoModal"
+          class="col-span-1 self-center text-info cursor-pointer hover:underline"
+        >
+          {{ $t('actions.edit') }}
+        </p>
       </span>
-      <span class="hover-trigger grid grid-cols-8 float-left mt-2 text-black-800">
-        <font-awesome-icon icon="fa-envelope" class="col-span-1 self-center text-lg p-3 bg-transparent rounded-md" />
+      <span
+        class="hover-trigger grid grid-cols-8 float-left mt-2 text-black-800"
+      >
+        <font-awesome-icon
+          icon="fa-envelope"
+          class="col-span-1 self-center text-lg p-3 bg-transparent rounded-md"
+        />
         <span class="col-span-2 -ml-4 float-left text-lg text-black-800">
-          <p class="text-sm">{{$t('rightpane.email_address')}}</p>
-          <p v-if='userProfileStore.user_profile?.contact_info?.email' id="email_address" class="text-sm text-info cursor-pointer hover:underline">{{userProfileStore.user_profile?.contact_info?.email}}</p>
-          <p v-if="ownProfile && !userProfileStore.user_profile?.contact_info?.email" class="text-sm text-info cursor-pointer hover:underline">{{$t('rightpane.add_email')}}</p>
+          <p class="text-sm">{{ $t('rightpane.email_address') }}</p>
+          <p
+            v-if="userProfileStore.user_profile?.contact_info?.email"
+            id="email_address"
+            class="text-sm text-info cursor-pointer hover:underline"
+          >
+            {{ userProfileStore.user_profile?.contact_info?.email }}
+          </p>
+          <p
+            v-if="
+              ownProfile && !userProfileStore.user_profile?.contact_info?.email
+            "
+            class="text-sm text-info cursor-pointer hover:underline"
+          >
+            {{ $t('rightpane.add_email') }}
+          </p>
         </span>
         <span class="col-span-4" />
         <n-tooltip placement="top" trigger="click">
           <template #trigger>
-            <font-awesome-icon v-if='userProfileStore.user_profile?.contact_info?.email' @click="copyEmailAddress" icon="fa-copy" class="hover-target col-span-1 self-center text-lg p-2 mr-2 float-right cursor-pointer bg-transparent rounded-md focus:outline-none" />
+            <font-awesome-icon
+              v-if="userProfileStore.user_profile?.contact_info?.email"
+              @click="copyEmailAddress"
+              icon="fa-copy"
+              class="hover-target col-span-1 self-center text-lg p-2 mr-2 float-right cursor-pointer bg-transparent rounded-md focus:outline-none"
+            />
           </template>
-          <span> {{$t('actions.copied')}} </span>
+          <span> {{ $t('actions.copied') }} </span>
         </n-tooltip>
       </span>
-      <span class="hover-trigger grid grid-cols-8 float-left text-lg mt-2 text-black-800">
-        <font-awesome-icon icon="fa-phone" class="col-span-1 self-center text-lg p-3 bg-transparent rounded-md" />
+      <span
+        class="hover-trigger grid grid-cols-8 float-left text-lg mt-2 text-black-800"
+      >
+        <font-awesome-icon
+          icon="fa-phone"
+          class="col-span-1 self-center text-lg p-3 bg-transparent rounded-md"
+        />
         <span class="col-span-2 -ml-4 float-left text-lg text-black-800">
-          <p class="text-sm">{{$t('rightpane.phone')}}</p>
-          <p v-if='userProfileStore.user_profile?.contact_info?.phone' id="phone_number" class="text-sm text-info cursor-pointer hover:underline">{{userProfileStore.user_profile?.contact_info?.phone}}</p>
-          <p v-if="ownProfile && !userProfileStore.user_profile?.contact_info?.phone" class="text-sm text-info cursor-pointer hover:underline">{{$t('rightpane.add_phone')}}</p>
+          <p class="text-sm">{{ $t('rightpane.phone') }}</p>
+          <p
+            v-if="userProfileStore.user_profile?.contact_info?.phone"
+            id="phone_number"
+            class="text-sm text-info cursor-pointer hover:underline"
+          >
+            {{ userProfileStore.user_profile?.contact_info?.phone }}
+          </p>
+          <p
+            v-if="
+              ownProfile && !userProfileStore.user_profile?.contact_info?.phone
+            "
+            class="text-sm text-info cursor-pointer hover:underline"
+          >
+            {{ $t('rightpane.add_phone') }}
+          </p>
         </span>
         <span class="col-span-4" />
         <n-tooltip placement="top" trigger="click">
           <template #trigger>
-            <font-awesome-icon v-if='userProfileStore.user_profile?.contact_info?.phone' @click="copyPhoneNumber" icon="fa-copy" class="hover-target col-span-1 self-center text-lg p-2 mr-2 float-right cursor-pointer bg-transparent rounded-md focus:outline-none" />
+            <font-awesome-icon
+              v-if="userProfileStore.user_profile?.contact_info?.phone"
+              @click="copyPhoneNumber"
+              icon="fa-copy"
+              class="hover-target col-span-1 self-center text-lg p-2 mr-2 float-right cursor-pointer bg-transparent rounded-md focus:outline-none"
+            />
           </template>
-          <span> {{$t('actions.copied')}} </span>
+          <span> {{ $t('actions.copied') }} </span>
         </n-tooltip>
       </span>
-
     </span>
-    <n-divider v-if="userProfileStore.user_profile?.about_me"/>
-    <span v-if="userProfileStore.user_profile?.about_me" class="flex flex-col float-left mx-4 mb-2 text-black-800">
+    <n-divider v-if="userProfileStore.user_profile?.about_me" />
+    <span
+      v-if="userProfileStore.user_profile?.about_me"
+      class="flex flex-col float-left mx-4 mb-2 text-black-800"
+    >
       <span class="grid grid-cols-12">
-        <p class="col-span-11 font-bold text-md break-words">{{$t('rightpane.about_me')}}</p>
-        <p v-if="ownProfile" class="col-span-1 self-center text-info cursor-pointer hover:underline">{{$t('actions.edit')}}</p>
+        <p class="col-span-11 font-bold text-md break-words">
+          {{ $t('rightpane.about_me') }}
+        </p>
+        <p
+          v-if="ownProfile"
+          class="col-span-1 self-center text-info cursor-pointer hover:underline"
+        >
+          {{ $t('actions.edit') }}
+        </p>
       </span>
       <span class="grid grid-cols-8 float-left mt-2 text-black-800">
         <span class="col-span-2 float-left text-md text-black-800">
-          <p class="text-sm">{{$t('rightpane.skype')}}</p>
-          <p v-if="userProfileStore.user_profile?.about_me?.skype" class="text-sm text-info cursor-pointer hover:underline">{{userProfileStore.user_profile?.about_me?.skype}}</p>
-          <p v-if="ownProfile && !userProfileStore.user_profile?.about_me?.skype" class="text-sm text-info cursor-pointer hover:underline">{{$t('rightpane.add_skype')}}</p>
+          <p class="text-sm">{{ $t('rightpane.skype') }}</p>
+          <p
+            v-if="userProfileStore.user_profile?.about_me?.skype"
+            class="text-sm text-info cursor-pointer hover:underline"
+          >
+            {{ userProfileStore.user_profile?.about_me?.skype }}
+          </p>
+          <p
+            v-if="ownProfile && !userProfileStore.user_profile?.about_me?.skype"
+            class="text-sm text-info cursor-pointer hover:underline"
+          >
+            {{ $t('rightpane.add_skype') }}
+          </p>
         </span>
       </span>
     </span>
-      <EditContactInfoModal v-if="showContactInfoModal" v-model:show="showContactInfoModal" :toggleModal="toggleEditContactInfoModal" :userContactInfo='userProfileStore.user_profile.contact_info' />
+    <EditContactInfoModal
+      v-if="showContactInfoModal"
+      v-model:show="showContactInfoModal"
+      :toggleModal="toggleEditContactInfoModal"
+      :userContactInfo="userProfileStore.user_profile.contact_info"
+    />
   </div>
 </template>
 
@@ -102,22 +267,25 @@ import { NDivider, NTooltip } from 'naive-ui';
 import RightPaneHeader from './RightPaneHeader.vue';
 import { useUserProfileStore } from '../../../stores/useUserProfileStore';
 import { useCurrentProfileStore } from '../../../stores/useCurrentProfileStore';
-import EditContactInfoModal from '../../widgets/EditContactInfoModal.vue'
+import EditContactInfoModal from '../../widgets/EditContactInfoModal.vue';
 import { ref } from 'vue';
 export default {
   components: { RightPaneHeader, NDivider, NTooltip, EditContactInfoModal },
-  setup(){
-    const userProfileStore = useUserProfileStore()
-    const currentProfileStore = useCurrentProfileStore()
-    const showContactInfoModal = ref(false)
-    return { userProfileStore, currentProfileStore, showContactInfoModal }
+  setup() {
+    const userProfileStore = useUserProfileStore();
+    const currentProfileStore = useCurrentProfileStore();
+    const showContactInfoModal = ref(false);
+    return { userProfileStore, currentProfileStore, showContactInfoModal };
   },
-  computed:{
-    ownProfile(){
-      return this.currentProfileStore.currentProfile.id === this.userProfileStore.user_profile.id
-    }
+  computed: {
+    ownProfile() {
+      return (
+        this.currentProfileStore.currentProfile.id ===
+        this.userProfileStore.user_profile.id
+      );
+    },
   },
-  methods:{
+  methods: {
     copyEmailAddress() {
       let element = document.getElementById('email_address');
       this.copyTextClipBoard(element.textContent);
@@ -127,12 +295,12 @@ export default {
       this.copyTextClipBoard(element.textContent);
     },
     copyTextClipBoard(text) {
-      navigator.clipboard.writeText(text)
+      navigator.clipboard.writeText(text);
     },
-    toggleEditContactInfoModal(){
-      this.showContactInfoModal = !this.showContactInfoModal
-    }
-  }
+    toggleEditContactInfoModal() {
+      this.showContactInfoModal = !this.showContactInfoModal;
+    },
+  },
 };
 </script>
 
--- app/javascript/views/widgets/EditContactInfoModal.vue
+++ app/javascript/views/widgets/EditContactInfoModal.vue
@@ -6,11 +6,13 @@
     :bordered="false"
     size="huge"
   >
-    <template #header>{{$t('rightpane.edit_contact_information')}}</template>
+    <template #header>{{ $t('rightpane.edit_contact_information') }}</template>
     <form @submit.prevent="handleSubmit">
       <span class="flex items-center">
         <font-awesome-icon icon="fa-lock" class="mr-2" />
-        <label class="font-semibold self-center">{{$t('rightpane.email_address')}}</label>
+        <label class="font-semibold self-center">{{
+          $t('rightpane.email_address')
+        }}</label>
       </span>
       <input
         v-model="email"
@@ -19,7 +21,7 @@
         disabled
         class="border border-black-400 rounded w-150 h-12 text-black-900 mt-2 p-3 cursor-not-allowed focus:outline-none focus:bg-transparent"
       />
-      <label class="flex mt-6">{{$t('rightpane.phone')}}</label>
+      <label class="flex mt-6">{{ $t('rightpane.phone') }}</label>
       <input
         v-model="phone"
         type="phone-number"
@@ -31,13 +33,13 @@
         @click="saveChanges"
         class="bg-green-700 text-white mt-8 float-right border px-4 h-9 rounded hover:bg-green-800 focus:outline-none"
       >
-        {{$t('actions.save_changes')}}
+        {{ $t('actions.save_changes') }}
       </button>
       <button
         @click="toggleModal"
         class="bg-white text-black mt-8 mr-3 float-right border px-4 h-9 rounded hover:bg-transparent focus:outline-none"
       >
-        {{$t('actions.cancel')}}
+        {{ $t('actions.cancel') }}
       </button>
     </form>
   </n-modal>
@@ -63,22 +65,22 @@ export default {
       phone: '',
     };
   },
-  mounted(){
-    this.setContactInfo()
+  mounted() {
+    this.setContactInfo();
   },
-  beforeUnmount(){
-    this.email = null
-    this.phone = null
+  beforeUnmount() {
+    this.email = null;
+    this.phone = null;
   },
-  methods:{
-    setContactInfo(){
-      this.email = this.userContactInfo.email
-      this.phone = this.userContactInfo.phone
+  methods: {
+    setContactInfo() {
+      this.email = this.userContactInfo.email;
+      this.phone = this.userContactInfo.phone;
     },
-    saveChanges(){
-      this.userContactInfo.phone = this.phone
-      this.toggleModal()
-    }
-  }
+    saveChanges() {
+      this.userContactInfo.phone = this.phone;
+      this.toggleModal();
+    },
+  },
 };
 </script>
