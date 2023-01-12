f858431e43f6fe9023a27458bbd21ec7964c1caa
MuhammadHammadShahzad <hammad.shahzada@devsinc.com>
2023-01-12 17:14:13 +0500

    CU-86697BQ6K | Pin/Unpin Feature - Conflicts Bug resolved

--- app/models/conversation_message.rb
+++ app/models/conversation_message.rb
@@ -32,7 +32,7 @@ class ConversationMessage < ApplicationRecord
   end
 
   def message_content
-    message = model_basic_content
+    message = message_basic_content
     message[:pin] = { id: pin.id, pinned_by: pin.profile.username } if pin.present?
     message[:sender_avatar] = Rails.application.routes.url_helpers.rails_storage_proxy_url(profile.profile_image) if profile.profile_image.present?
     message[:attachments] = attach_message_attachments if message_attachments.present?
