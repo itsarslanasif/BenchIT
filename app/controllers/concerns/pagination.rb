0778e255181ea1b9285989d9feaeef7d0b66b8c1
MuhammadArshadHabib <arshad.habib@devsinc.com>
2023-01-13 19:19:43 +0500

    CU-86698nzax | Pagination items count increased to 30

--- app/controllers/concerns/pagination.rb
+++ app/controllers/concerns/pagination.rb
@@ -4,7 +4,7 @@ module Pagination
 
   included do
     def pagination_for_chat_messages(conversation_id, page_no)
-      @pagy, @messages = pagy(ConversationMessage.chat_messages(conversation_id), page: page_no || 1, items: 20)
+      @pagy, @messages = pagy(ConversationMessage.chat_messages(conversation_id), page: page_no || 1, items: 30)
     rescue StandardError
       @pagy = nil
     end
