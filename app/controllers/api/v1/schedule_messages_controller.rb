6b28b3a6cd050167cc9220fac8fe0cd7bdc31259
faisalshehzaddev <faisal.shehzad@devsinc.com>
2023-01-26 11:25:14 +0500

    CU-8669c87qv | schedule message authentication added.

--- app/controllers/api/v1/schedule_messages_controller.rb
+++ app/controllers/api/v1/schedule_messages_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::ScheduleMessagesController < Api::ApiController
-  before_action :set_schedule_message, only: %i[destroy]
+  include MemberShip
+
+  before_action :set_schedule_message, :authenticat_message, only: %i[destroy update]
 
   def index
     @messages = Current.profile.schedule_messages.includes(:bench_conversation, :profile)
@@ -8,14 +10,25 @@ class Api::V1::ScheduleMessagesController < Api::ApiController
   def destroy
     queue = Sidekiq::ScheduledSet.new
     queue.each do |job|
-      job.jid if job.jid == @schedule_message.job_id
+      next unless job.jid.eql?(@schedule_message.job_id)
+
+      job.delete
+      @schedule_message.destroy!
+      break
     end
-    @schedule_message.destroy!
   end
 
   private
 
   def set_schedule_message
     @schedule_message = ScheduleMessage.find(params[:id])
+  end
+
+  def authenticat_message
+    if @schedule_message.profile_id.eql?(Current.profile.id)
+      check_membership(@schedule_message.bench_conversation)
+    else
+      render json: { error: 'Sorry, this message is not yours.' }, status: :unauthorized
+    end
   end
 end
