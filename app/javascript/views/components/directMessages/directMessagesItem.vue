c764b0581679eedfc27ad7231e394359adfa9a51
Asad170170 <asadullah.siddiqui@devsinc.com>
2023-01-27 00:37:23 +0500

    CU-8669d6hjb | active status added to dmList

--- app/javascript/views/components/directMessages/directMessagesItem.vue
+++ app/javascript/views/components/directMessages/directMessagesItem.vue
@@ -1,8 +1,22 @@
 <template>
-  <div v-if="user" @click="goToChat(`/profiles/${user.id}`, user)"
+  <div
+    v-if="user"
+    @mouseleave="changeActive"
+    @click="goToChat(`/profiles/${user.id}`, user)"
     class="flex items-center pl-2 py-1 hover:bg-primaryHover cursor-pointer"
-    :class="isUnreadDM(user) ? 'font-bold' : ''">
+    :class="isUnreadDM(user) ? 'font-bold' : ''"
+  >
     <n-avatar :size="25" :src="user.image_url" />
+    <div class="flex h-3 w-3 z-10">
+      <div
+        v-if="user.is_active"
+        class="bg-green-700 awayStatus text-black-800 inactivePosition h-2 w-2 rounded-xl"
+      />
+      <div
+        v-else
+        class="bg-black-800 awayStatus text-black-800 inactivePosition h-2 w-2 border-2 border-white rounded-xl"
+      />
+    </div>
     <p class="ml-2 text-sm text-white truncate">{{ user.username }}</p>
     <p v-if="isOwnChat(user)" class="ml-2 text-sm text-black-400">
       {{ $t('pinconversation.you') }}
@@ -21,7 +35,10 @@
         {{ statusClearAfterTime(user?.status?.clear_after) }}
       </span>
     </n-tooltip>
-    <div v-if="unreadDetails.messages.length" class="px-2 py-auto rounded-full text-xs bg-successHover ml-auto mr-2">
+    <div
+      v-if="unreadDetails.messages.length"
+      class="px-2 py-auto rounded-full text-xs bg-successHover ml-auto mr-2"
+    >
       {{ unreadDetails.messages.length }}
     </div>
   </div>
@@ -32,6 +49,7 @@ import { storeToRefs } from 'pinia';
 import { unreadMessagesCount } from '../../../modules/unreadMessages';
 import { NAvatar, NTooltip } from 'naive-ui';
 import moment from 'moment';
+
 export default {
   components: { NAvatar },
   props: ['user', 'isOwnChat', 'goToChat'],
@@ -49,6 +67,11 @@ export default {
       unreadMessages,
     };
   },
+  computed: {
+    profileActiveStatus() {
+      return true;
+    },
+  },
   methods: {
     isUnreadDM(user) {
       this.unreadDetails = unreadMessagesCount(
@@ -60,6 +83,17 @@ export default {
     statusClearAfterTime(time) {
       return time ? moment(time).calendar() : moment().endOf('month').fromNow();
     },
+    changeActive() {
+      console.log('actice', this.user.is_active);
+    },
   },
 };
 </script>
+<style>
+.awayStatus {
+  margin-left: -5px;
+  margin-top: 10px;
+  font-size: 6px;
+  outline: 3px solid rgb(31, 41, 55);
+}
+</style>
