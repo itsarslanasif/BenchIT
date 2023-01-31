185aa203386c9401a3271776943606ac1b6fa57c
amnakhalidd <amna.naseem@devsinc.com>
2023-01-31 20:49:38 +0500

    CU-8669cj9wp | Implemented suggested changes

--- app/models/concerns/avatar_generation.rb
+++ app/models/concerns/avatar_generation.rb
@@ -2,7 +2,17 @@ module AvatarGeneration
   extend ActiveSupport::Concern
 
   def generate_avatar(name, image)
-    uri = URI("https://ui-avatars.com/api/?name=#{name[0]}&background=random&id=#{id}")
+    params = {
+      name: name,
+      background: 'random',
+      length: 1,
+      bold: true,
+      id: id,
+      color: 'ffffff',
+      'font-size': 0.6
+    }
+    url = "#{ENV['UI_AVATAR_URL']}/?#{params.to_query}"
+    uri = URI(url)
     avatar = Net::HTTP.get(uri)
     image.attach(io: StringIO.new(avatar), filename: "#{name}.png", content_type: 'image/png')
   end
