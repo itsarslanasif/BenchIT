4358a2352e516af514c03051d60ecd33c93c75a4
faisalshehzaddev <faisal.shehzad@devsinc.com>
2023-02-01 14:42:26 +0500

    CU-8669cjuwh | model level associations and validations added

--- app/models/draft_message.rb
+++ app/models/draft_message.rb
@@ -1,7 +1,8 @@
 class DraftMessage < ApplicationRecord
   belongs_to :profile
   belongs_to :bench_conversation
+  belongs_to :conversation_message, optional: true
 
-  validates :content, presence: true
-  validates :bench_conversation, uniqueness: { scope: [:profile] }
+  validates :content, presence: true, length: { minimum: 1 }
+  validates :profile, uniqueness: { scope: %i[bench_conversation_id conversation_message_id] }
 end
