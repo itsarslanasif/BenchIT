ba2ad193f327b3ce2fdd1fb99bbb39e8eda48bbb
Muhammad Umar <umar.yasir@devsinc.com>
2023-02-01 21:41:23 +0500

    CU-3w8wm0c | Requested Changes

--- db/migrate/20230131103317_add_muted_channels_to_profile.rb
+++
@@ -1,5 +0,0 @@
-class AddMutedChannelsToProfile < ActiveRecord::Migration[7.0]
-  def change
-    add_column :profiles, :muted_channels, :integer, array: true, default: []
-  end
-end
--- db/migrate/20230131103317_add_muted_to_channel_participants.rb
+++ db/migrate/20230131103317_add_muted_to_channel_participants.rb
@@ -1,6 +1,5 @@
 class AddMutedToChannelParticipants < ActiveRecord::Migration[7.0]
   def change
-    remove_column :profiles, :muted_channels, :integer
     add_column :channel_participants, :muted, :boolean, default: false, null: false
   end
 end
