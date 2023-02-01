d561dacfc41819b5d6300bfb3a3376133adb8ab2
faisalshehzaddev <faisal.shehzad@devsinc.com>
2023-02-01 16:47:48 +0500

    CU-8669cjuwh | create draft method added along with authentication

--- app/controllers/api/v1/draft_messages_controller.rb
+++ app/controllers/api/v1/draft_messages_controller.rb
@@ -1,17 +1,15 @@
 class Api::V1::DraftMessagesController < Api::ApiController
-  before_action :set_draft_message, only: %i[update destroy]
+  include MemberShip
+
+  before_action :fetch_conversation, :initialize_draft, only: %i[create]
+  before_action :set_draft_message, :authenticate_draft, only: %i[destroy update]
+
   def index
     @draft_messages = Current.profile.draft_messages
   end
 
   def create
-    @draft_message = DraftMessage.new(draft_messages_params)
-
-    if @draft_message.save
-      render json: { message: 'Draft Message successfully created', draft_message: @draft_message }, status: :ok
-    else
-      render json: { error: 'Unable to create draft message', errors: @draft_message.errors }, status: :unprocessable_entity
-    end
+    @draft_message.save!
   end
 
   def update
@@ -34,11 +32,40 @@ class Api::V1::DraftMessagesController < Api::ApiController
 
   def set_draft_message
     @draft_message = DraftMessage.find(params[:id])
+    @bench_conversation = @draft_message.bench_conversation
   end
 
   def draft_messages_params
-    params.require(:draft_message).permit(:content, :bench_conversation_id).tap do |param|
+    params.require(:draft_message).permit(:content, :bench_conversation_id, :conversation_message_id).tap do |param|
       param[:profile] = Current.profile
     end
+  end
+
+  def authenticate_draft
+    if @draft_message.profile_id.eql?(Current.profile.id)
+      check_membership(@bench_conversation)
+    else
+      render json: { error: 'Sorry, this draft is not yours' }, status: :unauthorized
+    end
+  end
+
+  def initialize_draft
+    @draft_message = @bench_conversation.draft_messages.new(draft_messages_params)
+    check_membership(@bench_conversation)
+    render json: { error: 'Sorry, this draft is not present in this conversation' }, status: :unauthorized unless @bench_conversation.eql?(@draft_message.conversation_message.bench_conversation)
+  end
+
+  def fetch_conversation
+    conversation_id = params[:conversation_id]
+
+    @bench_conversation = case params[:conversation_type]
+                          when 'channels'
+                            BenchChannel.find_by(id: conversation_id)&.bench_conversation
+                          when 'groups'
+                            Group.find_by(id: conversation_id)&.bench_conversation
+                          when 'profiles'
+                            BenchConversation.profile_to_profile_conversation(Current.profile.id, conversation_id)
+                          end
+    render json: { error: 'wrong type' }, status: :bad_request if @bench_conversation.blank?
   end
 end
---
+++ app/views/api/v1/draft_messages/create.json.jbuilder
@@ -0,0 +1,1 @@
+json.partial! 'api/v1/draft_messages/partials/draft_message', message: @draft_message
