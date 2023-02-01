bb03cef97328202b1ce3f4681d71da784a42233d
faisalshehzaddev <faisal.shehzad@devsinc.com>
2023-02-01 15:53:52 +0500

    CU-8669cjuwh | helper methed rename

--- app/helpers/draft_messages_helper.rb
+++ app/helpers/draft_messages_helper.rb
@@ -1,10 +1,10 @@
 module DraftMessagesHelper
-  def any_draft(id)
+  def get_draft(id)
     DraftMessage.find_by(profile_id: Current.profile.id, bench_conversation_id: id, conversation_message_id: nil)
   end
 
-  def profile_draft(id)
+  def get_profile_draft(id)
     conversation = BenchConversation.profile_to_profile_conversation(Current.profile.id, id)
-    any_draft(conversation.id) if conversation.present?
+    get_draft(conversation.id) if conversation.present?
   end
 end
--- app/views/api/v1/profiles/show.json.jbuilder
+++ app/views/api/v1/profiles/show.json.jbuilder
@@ -1,2 +1,2 @@
 json.partial! 'api/v1/shared/partials/profile', profile: @profile
-json.draft_message any_draft(@profile.id)
+json.draft_message get_profile_draft(@profile.id)
