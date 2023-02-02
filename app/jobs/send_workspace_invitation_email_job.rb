4b75305821691eae72d40820ef8da56896859936
Muhammad Umar <umar.yasir@devsinc.com>
2023-02-02 19:56:01 +0500

    CU-8669cjauy_Invite_people_to_workspace - Removed unnecessary job.

--- app/jobs/send_workspace_invitation_email_job.rb
+++
@@ -1,7 +0,0 @@
-class SendWorkspaceInvitationEmailJob < ApplicationJob
-  queue_as :default
-
-  def perform(email, workspace, token)
-    WorkspaceMailer.send_email(email, workspace, token).deliver!
-  end
-end
