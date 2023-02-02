28ba8b09342b34a97fef9b8141a494fa69e623fa
faisalshehzaddev <faisal.shehzad@devsinc.com>
2023-02-02 17:36:14 +0500

    CU-8669cjuwh | naming convention improved.

--- app/controllers/api/v1/draft_messages_controller.rb
+++ app/controllers/api/v1/draft_messages_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::DraftMessagesController < Api::ApiController
   include MemberShip
   include Conversation
 
-  before_action :fetch_conversation, :initialize_draft, :check_draft, only: %i[create]
+  before_action :fetch_conversation, :initialize_draft, :verify_draft, :decide_draft_action, only: %i[create]
   before_action :set_draft_message, :authenticate_draft, only: %i[destroy update]
 
   def index
@@ -38,13 +38,13 @@ class Api::V1::DraftMessagesController < Api::ApiController
 
   def authenticate_draft
     if @draft_message.profile_id.eql?(Current.profile.id)
-      check_membership(@bench_conversation)
+      check_membership(@bench_conversatdecide_draft_actionion)
     else
       render json: { error: 'Sorry, this draft is not yours' }, status: :unauthorized
     end
   end
 
-  def check_draft
+  def decide_draft_action
     @message = DraftMessage.get_draft_message(@bench_conversation, params[:conversation_message_id])
     if params[:content].blank? && @message.present?
       @draft_message = @message
@@ -55,17 +55,19 @@ class Api::V1::DraftMessagesController < Api::ApiController
     elsif params[:content].present?
       create
     else
-      render json: { error: 'Sorry, the content is empty.' }, status: :unauthorized
+      render json: { error: 'Sorry, the content is empty.' }, status: :bad_request
     end
   end
 
   def initialize_draft
     @draft_message = @bench_conversation.draft_messages.new(draft_messages_params)
+  end
+
+  def verify_draft
     check_membership(@bench_conversation)
     return if @draft_message.conversation_message_id.blank? || @bench_conversation.eql?(@draft_message.conversation_message.bench_conversation)
 
-    render json: { error: 'Sorry, this draft is not present in this conversation' },
-           status: :unauthorized
+    render json: { error: 'Sorry, this draft is not present in this conversation' }, status: :unauthorized
   end
 
   def fetch_conversation
