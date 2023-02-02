00d402a8322d321c235ff1cda504d4820703047a
Ali Aftab <ali.aftab@devsinc.com>
2023-02-02 19:15:45 +0500

    CU-3U4bW8N | Search Functionality - Completed

--- app/javascript/views/components/search/AllSearches.vue
+++ app/javascript/views/components/search/AllSearches.vue
@@ -3,11 +3,19 @@
     <div class="border-b font-bold py-3 px-5 text-base">
       {{ `${$t('search.result_for')} "${searched}"` }}
     </div>
-    <div class="border-b py-1 px-5 text-sm flex gap-12">
+    <div class="border-b px-5 text-sm flex gap-12">
       <SearchCount :category="searches.messages" :heading="$t('search_bar.messages')"
-        @click="enableMessages($t('search_bar.messages'))" class="cursor-pointer" />
-      <SearchCount :category="searches.profiles" :heading="$t('search_bar.people')" />
-      <SearchCount :category="searches.channels" :heading="$t('search_bar.channels')" />
+        @click="changeEnabled($t('search_bar.messages'))" class="cursor-pointer py-2 px-5"
+        :class="enabled.Messages ? 'border-b-2 border-primary font-semibold' : ''" />
+      <SearchCount :category="searches.profiles" :heading="$t('search_bar.people')"
+        @click="changeEnabled($t('search_bar.profiles'))" class="cursor-pointer py-2 px-5"
+        :class="enabled.Profiles ? 'border-b-2 border-primary font-semibold' : ''" />
+      <SearchCount :category="searches.channels" :heading="$t('search_bar.channels')"
+        @click="changeEnabled($t('search_bar.channels'))" class="cursor-pointer py-2 px-5"
+        :class="enabled.Channels ? 'border-b-2 border-primary font-semibold' : ''" />
+    </div>
+    <div v-if="searchedList">
+      <SearchWrapper :list="searchedList" :listType="listType" />
     </div>
   </div>
 </template>
@@ -15,18 +23,22 @@
 import { storeToRefs } from 'pinia';
 import { useSearchStore } from '../../../stores/useSearchStore';
 import SearchCount from '../../widgets/SearchCount.vue';
+import SearchWrapper from '../../widgets/SearchWrapper.vue'
 export default {
   data() {
     return {
       enabled: {
-        People: false,
+        Profiles: false,
         Messages: false,
         Channels: false,
-      }
+      },
+      searchedList: this.searches.messages,
+      listType: this.$t('search_bar.messages')
     }
   },
   components: {
-    SearchCount
+    SearchCount,
+    SearchWrapper
   },
   setup() {
     const searchStore = useSearchStore()
@@ -37,8 +49,12 @@ export default {
     }
   },
   methods: {
-    enableMessages(category) {
-      this.enabled[category] = true
+    changeEnabled(key) {
+      for (const k in this.enabled) {
+        this.enabled[k] = k === key;
+      }
+      this.listType = key
+      this.searchedList = this.searches[key.toLowerCase()]
     }
   },
 }
---
+++ app/javascript/views/widgets/SearchWrapper.vue
@@ -0,0 +1,90 @@
+<template lang="">
+  <div class="p-3">
+    <div v-if="list.length" v-for="item in list" class="my-4">
+      <div class="flex gap-3 bg-slate-50 border border-black-300 rounded p-2" v-if="listType === $t('search_bar.messages')" >
+        <div class="align-baseline">
+          <n-avatar :src="item.sender_avatar" class="w-12 h-12 align-middle" />
+        </div>
+        <div>
+          <div class="font-bold">
+            {{ item.sender_name }}
+          </div>
+          <div>
+            {{ item.content }}
+          </div>
+        </div>
+      </div>
+      <div class="flex gap-3 bg-slate-50 border border-black-300 rounded p-2" v-if="listType === $t('search_bar.profiles')" @click="goToChat(`/profiles/${item.id}`)"  >
+        <div class="align-baseline">
+          <n-avatar :src="item.image_url" class="w-12 h-12 align-middle" />
+        </div>
+        <div>
+          <div class="font-bold capitalize">
+            {{ item.username }}
+          </div>
+          <div>
+            {{ item.description }}
+          </div>
+        </div>
+      </div>
+      <div class="flex gap-3 bg-slate-50 border border-black-300 rounded p-2" v-if="listType === $t('search_bar.channels')" @click="goToChat(`/channels/${item.id}`)" >
+        <div class="bg-slate-100 w-12 h-12 rounded flex items-center justify-center text-xl">
+            <font-awesome-icon :icon="getIcon(item)" />
+          </div>
+          <div>
+            <div class="font-bold capitalize">
+              {{ item.name }}
+            </div>
+            <div>
+              {{ item.description }}
+            </div>
+          </div>
+      </div>
+    </div>
+    <div v-else class="text-center text-base flex flex-col my-5">
+      <div class="text-4xl">
+        <font-awesome-icon icon="fa-magnifying-glass" />
+      </div>
+      <div>
+        {{ $t('search.no_result') }}
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { NAvatar } from 'naive-ui';
+import { useLeftpaneStore } from '../../stores/useLeftpaneStore';
+export default {
+  components: {
+    NAvatar
+  },
+  props: {
+    list: {
+      type: Array
+    },
+    listType: {
+      type: String
+    }
+  },
+  setup() {
+    const leftPaneStore = useLeftpaneStore()
+    return {
+      leftPaneStore
+    }
+  },
+  methods: {
+    getIcon(item) {
+      return `fa-${item.is_private ? 'lock' : 'hashtag'}`
+    },
+    goToChat(url) {
+      this.$router.push(url);
+      if (this.isMobileView()) {
+        this.leftPaneStore.closeLeftPane();
+      }
+    },
+    isMobileView() {
+      return window.innerWidth < 1400;
+    },
+  }
+}
+</script>
