76d8358709024691f3a8581e1c85dc0490d5d4e7
Asad170170 <asadullah.siddiqui@devsinc.com>
2023-02-07 01:43:03 +0500

    CU-3r5ekzm | implementation complete

--- app/javascript/views/components/editor/Attachment&shortCutRow.vue
+++ app/javascript/views/components/editor/Attachment&shortCutRow.vue
@@ -10,13 +10,16 @@
 
 <script>
 import { useShortcutAttachmentStore } from '../../../stores/useShortcut&AttachmentStore';
+import { useRecentFilesStore } from '../../../stores/useRecentFilesStore';
 import { ref } from 'vue';
 export default {
   props: ['object'],
   setup() {
     const ShortcutAndAttachmentStore = useShortcutAttachmentStore();
+    const fileStore = useRecentFilesStore();
     return {
       ShortcutAndAttachmentStore,
+      fileStore,
     };
   },
   data() {
@@ -28,6 +31,8 @@ export default {
     onSelect(object) {
       if (object.label === 'Create a text snippet') {
         this.ShortcutAndAttachmentStore.toggleShowCreateTextSnippitModal();
+        this.fileStore.showModalInThread = false;
+        this.fileStore.showModalInChat = false;
       }
     },
   },
--- app/javascript/views/components/editor/CreateTextSnippetModal.vue
+++ app/javascript/views/components/editor/CreateTextSnippetModal.vue
@@ -19,48 +19,74 @@
         </template>
         <template #footer>
           <n-space class="flex" vertical>
-            <p class="">Titial optional</p>
-            <n-input
-              v-model:value="title"
-              type="text"
-              placeholder="sinippet.txt"
-            />
-            <p>Content</p>
-            <n-input
-              v-model:value="content"
-              type="textarea"
-              placeholder="..."
-            />
-            <n-checkbox size="small" label="Wrap" />
-            <n-input
-              v-model:value="value"
-              
-              type="textarea"
-              placeholder="Add a message, if you'd like."
-            />
-            <n-checkbox size="small" label="Share this file" />
-            <n-select
-              vertical
-              v-model:value="selectedValues"
-              filterable
-              :placeholder="$t('placeholder.add_people_to_channel')"
-              :options="options"
-              :loading="loading"
-              clearable
-              remote
-              :clear-filter-after-select="true"
-              :show-arrow="false"
-              @search="handleSearch"
-              size="large"
-              @change="resetSelectedTag()"
-            />
-            <n-button
-              @click="createTextFile"
-              class="float-right"
-              type="success"
+            <n-form
+              :label-width="80"
+              :model="formValue"
+              :rules="rules"
+              :size="size"
             >
-              Create snippet
-            </n-button>
+              <n-form-item label="Titial (optional)" path="Titial">
+                <n-input
+                  v-model:value="formValue.title"
+                  :maxlength="80"
+                  :count-graphemes="countGraphemes"
+                  show-count
+                  placeholder="sinippet.txt"
+                />
+              </n-form-item>
+
+              <n-form-item label="Content" path="content">
+                <n-input
+                  v-model:value="formValue.content"
+                  type="textarea"
+                  placeholder="Add content here."
+                />
+              </n-form-item>
+              <n-checkbox
+                v-model:checked="formValue.wrap"
+                class="mb-5"
+                size="small"
+                label="Wrap"
+              />
+              <n-form-item label="Wrapper" path="Titial">
+                <n-input
+                  v-model:value="formValue.message"
+                  type="textarea"
+                  placeholder="Add a message, if you'd like."
+                />
+              </n-form-item>
+              <n-checkbox
+                v-model:checked="formValue.share"
+                size="small"
+                label="Share this file"
+              />
+              <n-form-item path="Titial">
+                <n-select
+                  vertical
+                  v-model:value="selectedValues"
+                  filterable
+                  :placeholder="$t('placeholder.add_people_to_channel')"
+                  :options="options"
+                  :loading="loading"
+                  clearable
+                  remote
+                  :clear-filter-after-select="true"
+                  :show-arrow="false"
+                  @search="handleSearch"
+                  size="large"
+                  @change="resetSelectedTag()"
+                />
+              </n-form-item>
+              <n-form-item class="float-right">
+                <n-button
+                  :disabled="disableButton"
+                  @click="createTextFile"
+                  class="bg-success text-white py-2 px-5 text-base float-right my-3 rounded"
+                >
+                  {{ $t('actions.create') }}
+                </n-button>
+              </n-form-item>
+            </n-form>
           </n-space>
         </template>
       </n-card>
@@ -81,6 +107,9 @@ import {
   NButton,
   NAvatar,
   NSelect,
+  NForm,
+  NFormItem,
+  NSwitch,
 } from 'naive-ui';
 import { useShortcutAttachmentStore } from '../../../stores/useShortcut&AttachmentStore';
 import { ref, h } from 'vue';
@@ -99,19 +128,47 @@ export default {
     NButton,
     NAvatar,
     NSelect,
+    NForm,
+    NFormItem,
+    NSwitch,
   },
   data() {
     return {
-      title: '',
-      content: '',
-      wrap: 'true',
-      message: '',
-      share: '',
+      rules: {
+        content: {
+          required: true,
+          message: "content can't be empty",
+          trigger: ['input'],
+        },
+      },
+      disableButton: ref(true),
+      formValue: {
+        title: '',
+        content: '',
+        wrap: false,
+        share: true,
+        message: '',
+      },
+      error: '',
+      share: 'true',
+      lable: 'lable',
 
       loading: false,
       options: [],
       selectedValues: null,
     };
+  },
+  watch: {
+    formValue: {
+      handler(newValue) {
+        if (newValue.content != '') {
+          this.disableButton = false;
+        } else {
+          this.disableButton = true;
+        }
+      },
+      deep: true,
+    },
   },
   mounted() {
     this.selectedValues = this.selectedChat.username || this.selectedChat.name;
@@ -143,12 +200,16 @@ export default {
       this.value = '';
     },
     createTextFile() {
-      const textFile = new Blob([this.content], { type: 'text/plain' });
+      const textFile = new Blob([this.formValue.content], {
+        type: 'text/plain',
+      });
       const files = [textFile];
-      if (this.title !== '') {
-        this.sendMessage('FILE', files, this.title);
-      } else {
-        this.sendMessage('FILE', files, 'textFile.txt');
+      if (this.formValue.share) {
+        if (this.formValue.title !== '') {
+          this.sendMessage('FILE', files, this.formValue.title);
+        } else {
+          this.sendMessage('FILE', files, 'textFile.txt');
+        }
       }
       this.shortcutAttachmentStore.toggleShowCreateTextSnippitModal();
     },
