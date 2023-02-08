35dfda1987bceaf5f29012621c50406263349142
Muhammad Umar <umar.yasir@devsinc.com>
2023-02-08 23:54:47 +0500

    CU-3wu45hb | Requested changes

--- app/controllers/api/v1/dm_users_controller.rb
+++ app/controllers/api/v1/dm_users_controller.rb
@@ -1,17 +1,17 @@
 class Api::V1::DmUsersController < Api::ApiController
-  before_action :set_dm, only: %i[destroy]
-  before_action :set_dm_list, only: %i[index]
+  before_action :set_direct_message_user, only: %i[destroy]
+  before_action :set_direct_message_list, only: %i[index]
   before_action :find_last_messages_users, only: %i[last_direct_messages]
 
   def index
-    @profiles = Profile.where(id: @current_profile.dm_users.pluck(:receiver_id) & @dm_users_ids)
+    @profiles = Profile.where(id: @current_profile.dm_users.pluck(:receiver_id) & @direct_message_users_ids)
   end
 
   def destroy
-    if @dm.destroy
+    if @direct_message_user.destroy
       render json: { message: t('.removed') }, status: :ok
     else
-      render json: { error: t('.unable_to_remove'), errors: @dm.errors }, status: :unprocessable_entity
+      render json: { errors: @direct_message_user.errors }, status: :unprocessable_entity
     end
   end
 
@@ -22,25 +22,25 @@ class Api::V1::DmUsersController < Api::ApiController
   def fetch_last_messages
     @last_messages_users.filter_map do |id|
       conversation = BenchConversation.profile_to_profile_conversation(@current_profile.id, id)
-      conversation&.conversation_messages&.last if conversation.present?
+      conversation.conversation_messages&.last if conversation.present?
     end.sort_by(&:created_at).reverse
   end
 
   private
 
-  def set_dm
-    @dm = @current_profile.dm_users.find_by(receiver_id: params[:id])
-    render json: { message: t('.not_found') }, status: :not_found if @dm.nil?
+  def set_direct_message_user
+    @direct_message_user = @current_profile.dm_users.find_by(receiver_id: params[:id])
+    render json: { message: t('.not_found') }, status: :not_found if @direct_message_user.nil?
   end
 
-  def set_dm_list
+  def set_direct_message_list
     conversation_ids = BenchConversation.recent_last_conversation
     return render json: [@current_profile] if conversation_ids.empty?
 
     bench_conversations_ids = ConversationMessage.recent_last_conversation(conversation_ids)
     return render json: [@current_profile] if bench_conversations_ids.empty?
 
-    @dm_users_ids = BenchConversation.where(id: bench_conversations_ids).pluck(:conversationable_id, :sender_id).flatten.uniq
+    @direct_message_users_ids = BenchConversation.where(id: bench_conversations_ids).pluck(:conversationable_id, :sender_id).flatten.uniq
   end
 
   def find_last_messages_users
