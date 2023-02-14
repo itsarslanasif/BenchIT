e8f82d3932e0f94c663414f5aa9bfdafcd93548a
faisalshehzaddev <faisal.shehzad@devsinc.com>
2023-02-14 23:21:56 +0500

    CU-8669h7kwq | pagination item number change

--- app/controllers/concerns/pagination.rb
+++ app/controllers/concerns/pagination.rb
@@ -21,17 +21,22 @@ module Pagination
     end
 
     def pagination_for_send_messages(page_no = 1)
-      @pagy, @messages = pagy(ConversationMessage.send_messages, page: page_no, items: 2)
+      @pagy, @messages = pagy(ConversationMessage.send_messages, page: page_no, items: 20)
       [@pagy, @messages]
     end
 
     def pagination_for_save_messages(page_no = 1)
-      @pagy, @messages = pagy(current_profile.saved_items.order(:created_at), page: page_no, items: 2)
+      @pagy, @messages = pagy(current_profile.saved_items.order(:created_at), page: page_no, items: 10)
       [@pagy, @messages]
     end
 
     def pagination_for_schedule_messages(page_no = 1)
-      @pagy, @messages = pagy(current_profile.schedule_messages.order(:scheduled_at), page: page_no, items: 2)
+      @pagy, @messages = pagy(current_profile.schedule_messages.order(:scheduled_at), page: page_no, items: 10)
+      [@pagy, @messages]
+    end
+
+    def pagination_for_draft_messages(page_no = 1)
+      @pagy, @messages = pagy(current_profile.draft_messages.order(:created_at), page: page_no, items: 20)
       [@pagy, @messages]
     end
   end
