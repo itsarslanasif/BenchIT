6acc839c7e68b45f4684d44fb97e0c8614adf115
asadtariq-dev <asad.tariq@devsinc.com>
2023-02-15 23:41:57 +0500

    CU-8669H8RF0 | Threads Styling
    - Removed extra margin over Text Editor

--- app/javascript/views/components/rightPane/Thread.vue
+++ app/javascript/views/components/rightPane/Thread.vue
@@ -29,7 +29,7 @@
         </template>
       </template>
     </div>
-    <div class="relative mx-1 mt-10">
+    <div class="relative mx-1">
       <TextEditorVue
         :isThread="true"
         :sendMessage="sendMessage"
