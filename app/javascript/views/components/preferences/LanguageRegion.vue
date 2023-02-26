e517721999854015b1331b41f7043964265aa2c0
Ali Aftab <ali.aftab@devsinc.com>
2023-02-26 13:21:04 +0500

    CU-3Y7W06V | Create Preferences Modal - Language & Region Settings: Completed

--- app/javascript/views/components/preferences/LanguageRegion.vue
+++ app/javascript/views/components/preferences/LanguageRegion.vue
@@ -1,13 +1,50 @@
 <template lang="">
   <div>
-    Language & Region
+    <div class="mb-10">
+      <p>
+        <strong>{{ languageRegion.language.title }}</strong>
+      </p>
+      <select class="w-full p-4 my-3">
+        <option v-for="item in languageRegion.language.settings.options">
+          {{ item.label }}
+        </option>
+      </select>
+      <p>{{ languageRegion.language.tagline }}</p>
+    </div>
+
+    <div class="my-10">
+      <p>
+        <strong>{{ languageRegion.timezone.title }}</strong>
+      </p>
+      <select class="w-full p-4 my-3">
+        <option v-for="item in languageRegion.timezone.settings.options">
+          {{ item.label }}
+        </option>
+      </select>
+      <p>{{ languageRegion.timezone.tagline }}</p>
+    </div>
+
+    <div class="my-10">
+      <p>
+        <strong>{{ languageRegion.spellCheck.title }}</strong>
+      </p>
+      <input
+        type="text"
+        class="w-full p-4 my-3"
+        :value="languageRegion.spellCheck.settings.selected[0]"
+      />
+      <p>{{ languageRegion.spellCheck.tagline }}</p>
+    </div>
   </div>
 </template>
+
 <script>
+import { languageRegion } from '../../../assets/settings/languageRegion';
 export default {
-  
-}
+  data() {
+    return {
+      languageRegion,
+    };
+  },
+};
 </script>
-<style lang="">
-  
-</style>
