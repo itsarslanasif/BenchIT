0b7e0d99275d49b6132a7d21897597882f9e50c5
faisalshehzaddev <faisal.shehzad@devsinc.com>
2023-03-01 21:03:21 +0500

    CU-8669n2bz2 | current_profile mistake corrected

--- app/controllers/api/v1/direct_message_users_controller.rb
+++ app/controllers/api/v1/direct_message_users_controller.rb
@@ -25,10 +25,10 @@ class Api::V1::DirectMessageUsersController < Api::ApiController
 
   def set_direct_message_list
     conversation_ids = BenchConversation.recent_conversation_ids
-    return render json: [profile_profile] if conversation_ids.empty?
+    return render json: [current_profile] if conversation_ids.empty?
 
     bench_conversations_ids = ConversationMessage.recent_conversation_ids(conversation_ids)
-    return render json: [profile_profile] if bench_conversations_ids.empty?
+    return render json: [current_profile] if bench_conversations_ids.empty?
 
     @direct_message_users_ids = BenchConversation.where(id: bench_conversations_ids).pluck(:conversationable_id, :sender_id).flatten.uniq
   end
