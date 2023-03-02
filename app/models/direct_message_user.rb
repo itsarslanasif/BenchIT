a4c9d511c92bfb908661ce6750bd70cc0583733a
Muhammad Umar <umar.yasir@devsinc.com>
2023-03-02 17:06:52 +0500

    Fixes_in_direct_message_list

--- app/models/direct_message_user.rb
+++ app/models/direct_message_user.rb
@@ -3,7 +3,6 @@ class DirectMessageUser < ApplicationRecord
   validates :profile, uniqueness: { scope: %i[receiver_id] }
 
   scope :dm_profiles, lambda { |direct_message_users_ids|
-                        Profile.where(id: Current.profile.direct_message_users.pluck(:receiver_id) & direct_message_users_ids) |
-                          [Current.profile]
+                        Profile.where(id: (Current.profile.direct_message_users.pluck(:receiver_id) & direct_message_users_ids) | [Current.profile.id])
                       }
 end
