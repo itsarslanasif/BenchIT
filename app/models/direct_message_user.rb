8235442a7e2ab64f5c1cb8eab14a588cd227f044
Muhammad Umar <umar.yasir@devsinc.com>
2023-03-02 17:08:47 +0500

    Fixes_in_direct_message_list - rubocop offenses resolved.

--- app/models/direct_message_user.rb
+++ app/models/direct_message_user.rb
@@ -3,6 +3,7 @@ class DirectMessageUser < ApplicationRecord
   validates :profile, uniqueness: { scope: %i[receiver_id] }
 
   scope :dm_profiles, lambda { |direct_message_users_ids|
-                        Profile.where(id: (Current.profile.direct_message_users.pluck(:receiver_id) & direct_message_users_ids) | [Current.profile.id])
+                        Profile.where(id: (Current.profile.direct_message_users.pluck(:receiver_id) & direct_message_users_ids) |
+                        [Current.profile.id])
                       }
 end
