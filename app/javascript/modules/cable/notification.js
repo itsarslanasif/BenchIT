8ba7b06fdc62d222d081a1af4ab93c6744a0d684
Ali Aftab <ali.aftab@devsinc.com>
2023-03-02 15:50:17 +0500

    CU-3R5EKUD | Push Notification - Completed

--- app/javascript/modules/cable/notification.js
+++ app/javascript/modules/cable/notification.js
@@ -5,6 +5,52 @@ import { useCurrentProfileStore } from '../../stores/useCurrentProfileStore';
 import { useProfileStore } from '../../stores/useProfileStore';
 import { useMessageStore } from '../../stores/useMessagesStore';
 
+const getMessageBody = (details, data) => {
+  let messageContent =
+    data.conversationable_type === 'BenchChannel'
+      ? `${data.sender_name}: `
+      : '';
+  const blocks = JSON.parse(data.content).blocks;
+  for (let key in blocks) {
+    messageContent += `${blocks[key].text.text} `;
+  }
+  return messageContent;
+};
+
+const showNotification = (details, data) => {
+  if (!('Notification' in window)) {
+    console.log('This browser does not support notifications.');
+    return;
+  }
+  const messageBody = getMessageBody(details, data);
+  Notification.requestPermission().then(permission => {
+    if (permission === 'granted') {
+      new Notification(
+        `New message from ${
+          details.creator_id ? details.name : details.username
+        }`,
+        {
+          body: messageBody,
+        }
+      );
+    }
+  });
+};
+
+const createPayloadForNotification = async data => {
+  let messageDetail;
+  if (data.conversationable_type === 'BenchChannel') {
+    const channels = useChannelStore().getChannels;
+    messageDetail = channels
+      .filter(channel => channel.id === data.conversationable_id)
+      .pop();
+  } else if (data.conversationable_type === 'Profile') {
+    const profileStore = useProfileStore();
+    messageDetail = profileStore.getProfileById(data.conversationable_id);
+  }
+  showNotification(messageDetail, data);
+};
+
 const createMessage = data => {
   const profiles = useProfileStore();
   const receiver = profiles.getProfileById(data.sender_id);
@@ -16,6 +62,7 @@ const createMessage = data => {
   };
   const conversation_type = getIndexByParams(1);
   const id = getIndexByParams(2);
+  createPayloadForNotification(data);
   try {
     if (!data.parent_message_id) {
       unreadMessagesStore.addNewMessage(data, conversation_type, id);
@@ -47,19 +94,21 @@ const ChannelParticipantDelete = async data => {
 };
 
 const updateProfileStatus = data => {
-  const { setProfileStatus, setProfileActiveStatus, currentProfile } = useCurrentProfileStore();
+  const { setProfileStatus, setProfileActiveStatus, currentProfile } =
+    useCurrentProfileStore();
   const { updateProfileStatus } = useProfileStore();
-  const { updateProfileStatus:updateDmProfileStatus } = useDirectMessagesStore();
+  const { updateProfileStatus: updateDmProfileStatus } =
+    useDirectMessagesStore();
   const { updateSelectedprofileStatus } = useMessageStore();
 
   if (currentProfile.id === data.id) {
     setProfileStatus(data.status);
-    setProfileActiveStatus(data.is_active)
+    setProfileActiveStatus(data.is_active);
   }
-    updateProfileStatus(data);
-    updateDmProfileStatus(data);
-    updateSelectedprofileStatus(data)
-  };
+  updateProfileStatus(data);
+  updateDmProfileStatus(data);
+  updateSelectedprofileStatus(data);
+};
 
 const notificationActions = {
   MessageCreate: createMessage,
