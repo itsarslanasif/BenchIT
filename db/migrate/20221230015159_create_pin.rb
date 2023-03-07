10c30cd4aa7cf4dae9ca73333e419998fd1bd60c
faisalshehzaddev <faisal.shehzad@devsinc.com>
2023-03-07 13:28:48 +0500

    CU-8669nj0pe | rubocop offenses remove

--- db/migrate/20221230015159_create_pin.rb
+++ db/migrate/20221230015159_create_pin.rb
@@ -1,9 +1,9 @@
 class CreatePin < ActiveRecord::Migration[7.0]
   def change
     create_table :pins, id: :uuid do |t|
-      t.references :conversation_message, null: false, foreign_key: true,type: :uuid, index: true
+      t.references :conversation_message, null: false, foreign_key: true, type: :uuid, index: true
       t.references :profile, null: false, foreign_key: true, type: :uuid, index: true
-      t.references :bench_conversation, null: false, foreign_key: true,type: :uuid, index: true
+      t.references :bench_conversation, null: false, foreign_key: true, type: :uuid, index: true
       t.timestamps
     end
   end
--- db/migrate/20230201093341_change_draft_message.rb
+++ db/migrate/20230201093341_change_draft_message.rb
@@ -1,7 +1,7 @@
 class ChangeDraftMessage < ActiveRecord::Migration[7.0]
   def up
     remove_index :draft_messages, name: 'index_draft_messages_on_profile_id_and_bench_conversation_id'
-    add_reference :draft_messages, :conversation_message, null: true, foreign_key: true,type: :uuid, index: true
+    add_reference :draft_messages, :conversation_message, null: true, foreign_key: true, type: :uuid, index: true
     add_index :draft_messages, [:profile_id, :bench_conversation_id, :conversation_message_id], unique: true, name: 'draft_message'
   end
 
