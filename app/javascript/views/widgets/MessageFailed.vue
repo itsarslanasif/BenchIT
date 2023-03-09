5b2c5abd8b4261cf63bfacc5a740ff9f1e49b6a7
Ali Aftab <ali.aftab@devsinc.com>
2023-03-09 15:54:56 +0500

    CU-8669NJ1E7 | No Internet Conectivity Behaviour - Alert is working

--- app/javascript/views/widgets/MessageFailed.vue
+++ app/javascript/views/widgets/MessageFailed.vue
@@ -2,7 +2,7 @@
   <div class="text-xs">
     <p class="text-gray-600 font-semibold">
       <font-awesome-icon icon="fa-xmark" class="pr-1 text-sm" />
-      Failed to send
+      {{ $t('connection.message_failed') }}
     </p>
     <div class="flex gap-3 my-2">
       <p
@@ -10,13 +10,14 @@
         class="hover:text-white hover:bg-danger text-danger py-0.5 px-2 border border-danger hover:border-transparent rounded duration-200 cursor-pointer"
       >
         <font-awesome-icon icon="fa-trash" class="pr-1" />
-        Delete this message
+        {{ $t('connection.delete') }}
       </p>
       <p
+        @click="retry"
         class="hover:text-white hover:bg-info text-info py-0.5 px-2 border border-info hover:border-transparent rounded duration-200 cursor-pointer"
       >
-        <font-awesome-icon icon="fa-arrow-rotate-right" class="pr-1"  />
-        Retry
+        <font-awesome-icon icon="fa-arrow-rotate-right" class="pr-1" />
+        {{ $t('connection.retry') }}
       </p>
     </div>
   </div>
@@ -24,22 +25,41 @@
 
 <script>
 import { useConnectionStore } from '../../stores/useConnectionStore.js';
+import { useMessage } from 'naive-ui';
 export default {
   props: {
     message: {
       type: Object,
-    }
+    },
   },
+
   setup() {
-    const connectionStore = useConnectionStore()
+    const connectionStore = useConnectionStore();
+    const message = useMessage();
+    
+    const error = errorMessage => {
+      message.error(errorMessage);
+    };
+
+    const success = (successMessage) => {
+      message.success(successMessage);
+    };
+
     return {
       connectionStore,
-    }
+      error,
+      success,
+    };
   },
+  
   methods: {
     deleteTheMessage() {
-      this.connectionStore.deleteTheMessage(this.message)
-    }
-  }
+      this.connectionStore.deleteTheMessage(this.message);
+      this.success(this.$t('connection.message_deleted_successfully'))
+    },
+    retry() {
+      this.error(this.$t('connection.not_connected_to_internet'));
+    },
+  },
 };
 </script>
