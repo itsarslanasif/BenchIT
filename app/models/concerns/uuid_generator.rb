d3711d0ea90fec2a8d0b9fe55d6513b0eb1edc00
faisalshehzaddev <faisal.shehzad@devsinc.com>
2023-03-09 14:18:12 +0500

    CU-8669nj0pe | id limit increses from 11 to 13

--- app/models/concerns/uuid_generator.rb
+++ app/models/concerns/uuid_generator.rb
@@ -4,21 +4,21 @@ module UuidGenerator
   def generate_and_append_uuid(record)
     record.id = case record.class.name
                 when 'Workspace'
-                  "W#{SecureRandom.alphanumeric(10).upcase}"
+                  "W#{SecureRandom.alphanumeric(12).upcase}"
                 when 'User'
-                  "U#{SecureRandom.alphanumeric(10).upcase}"
+                  "U#{SecureRandom.alphanumeric(12).upcase}"
                 when 'Profile'
-                  "D#{SecureRandom.alphanumeric(10).upcase}"
+                  "D#{SecureRandom.alphanumeric(12).upcase}"
                 when 'BenchChannel'
                   if record.is_private?
-                    "CPR#{SecureRandom.alphanumeric(8).upcase}"
+                    "CPR#{SecureRandom.alphanumeric(10).upcase}"
                   else
-                    "CPU#{SecureRandom.alphanumeric(8).upcase}"
+                    "CPU#{SecureRandom.alphanumeric(10).upcase}"
                   end
                 when 'Group'
-                  "G#{SecureRandom.alphanumeric(10).upcase}"
+                  "G#{SecureRandom.alphanumeric(12).upcase}"
                 else
-                  "M#{SecureRandom.alphanumeric(10).upcase}"
+                  "M#{SecureRandom.alphanumeric(12).upcase}"
                 end
   end
 end
