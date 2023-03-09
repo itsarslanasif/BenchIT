db5f8012fa701197efd082104fbbb23a1f35c314
faisalshehzaddev <faisal.shehzad@devsinc.com>
2023-03-09 14:37:45 +0500

    CU-8669nj0pe | 0 added in the id

--- app/models/concerns/uuid_generator.rb
+++ app/models/concerns/uuid_generator.rb
@@ -4,21 +4,21 @@ module UuidGenerator
   def generate_and_append_uuid(record)
     record.id = case record.class.name
                 when 'Workspace'
-                  "W#{SecureRandom.alphanumeric(12).upcase}"
+                  "W0#{SecureRandom.alphanumeric(12).upcase}"
                 when 'User'
-                  "U#{SecureRandom.alphanumeric(12).upcase}"
+                  "U0#{SecureRandom.alphanumeric(12).upcase}"
                 when 'Profile'
-                  "D#{SecureRandom.alphanumeric(12).upcase}"
+                  "D0#{SecureRandom.alphanumeric(12).upcase}"
                 when 'BenchChannel'
                   if record.is_private?
-                    "CPR#{SecureRandom.alphanumeric(10).upcase}"
+                    "C0PR#{SecureRandom.alphanumeric(10).upcase}"
                   else
-                    "CPU#{SecureRandom.alphanumeric(10).upcase}"
+                    "C0PU#{SecureRandom.alphanumeric(10).upcase}"
                   end
                 when 'Group'
-                  "G#{SecureRandom.alphanumeric(12).upcase}"
+                  "G0#{SecureRandom.alphanumeric(12).upcase}"
                 else
-                  "M#{SecureRandom.alphanumeric(12).upcase}"
+                  "M0#{SecureRandom.alphanumeric(12).upcase}"
                 end
   end
 end
