ddd7ecbed0cea919350dc6ec3a10240f72d88faa
Muhammad Umar <umar.yasir@devsinc.com>
2023-03-09 18:19:22 +0500

    CU-8669nj0pe | update type of draft message.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_09_104837) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_131437) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -129,7 +129,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_09_104837) do
   end
 
   create_table "draft_messages", id: :string, force: :cascade do |t|
-    t.text "content", null: false
+    t.json "content", null: false
     t.uuid "bench_conversation_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
