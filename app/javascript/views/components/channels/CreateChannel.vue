bec42fa4e19917ac704505a94e5e84602860fe15
Ali Ahmad <ali.zafar@devsinc.com>
2023-03-20 17:49:44 +0500

    CU-8669nj07m | fixed some code in CreateChannel

--- app/javascript/views/components/channels/CreateChannel.vue
+++ app/javascript/views/components/channels/CreateChannel.vue
@@ -1,16 +1,23 @@
 <template>
   <div
-    class="flex justify-center items-center z-10 left-0 right-0 top-0 bottom-0 fixed bg-opacity-25 bg-backgroundTransparent">
-    <div v-click-outside="closeModal"
-      class="rounded-md sm:w-flexible-lg md:w-flexible-lg lg:w-150 w-80 shadow-md bg-white" role="dialog">
+    class="flex justify-center items-center z-10 left-0 right-0 top-0 bottom-0 fixed bg-opacity-25 bg-backgroundTransparent overflow-auto"
+  >
+    <div
+      v-click-outside="closeModal"
+      class="rounded-md sm:w-flexible-lg md:w-flexible-lg lg:w-150 w-80 shadow-md bg-white"
+      role="dialog"
+    >
       <div class="p-8">
-        <header id="modalTitle" class="flex w-full">
-          <div class="w-5/6 text-lg text-black-900">
+        <header id="modalTitle" class="flex justify-between">
+          <div class="text-lg text-black-900">
             <h1>{{ $t('channels.create_channel') }}</h1>
           </div>
-          <button type="button" class="w-1/6 bg-none py-1 px-3 text-base float-right text-black-900 rounded"
-            @click="closeModal">
-            {{ $t('actions.close') }}
+          <button
+            type="button"
+            class="focus:outline-none bg-none py-1 px-2 text-base float-right text-black-900 rounded"
+            @click="closeModal"
+          >
+            <font-awesome-icon icon="fa-solid fa-xmark" />
           </button>
         </header>
         <div class="m-0 relative mt-5">
@@ -20,53 +27,45 @@
             </p>
           </div>
           <n-form :label-width="80" :model="formValue" :rules="rules">
-            <n-form-item :label="$t('channels.new_channel_name')" path="channelName">
-              <n-input v-model:value="formValue.channelName" :maxlength="80" :count-graphemes="countGraphemes" show-count
-                :placeholder="$t('channels.new_channel_name_placeholder')" />
+            <n-form-item
+              :label="$t('channels.new_channel_name')"
+              path="channelName"
+            >
+              <n-input
+                v-model:value="formValue.channelName"
+                :maxlength="80"
+                :count-graphemes="countGraphemes"
+                show-count
+                :placeholder="$t('channels.new_channel_name_placeholder')"
+              />
             </n-form-item>
-            <n-form-item class="mt-3" :label="$t('channels.new_channel_description')" path="Description">
-              <n-input v-model:value="formValue.Description" :maxlength="500" :count-graphemes="countGraphemes" show-count
-                :placeholder="$t('channels.new_channel_desc_placeholder')" />
+            <n-form-item
+              class="mt-3"
+              :label="$t('channels.new_channel_description')"
+              path="Description"
+            >
+              <n-input
+                v-model:value="formValue.Description"
+                :maxlength="500"
+                :count-graphemes="countGraphemes"
+                show-count
+                :placeholder="$t('channels.new_channel_desc_placeholder')"
+              />
             </n-form-item>
             <p v-if="error" class="mt-3 text-danger">{{ error }}</p>
             <n-form-item class="float-right">
-              <n-button @click="onSubmit" class="bg-success text-white py-2 px-5 text-base float-right my-3 rounded">
+              <n-button
+                @click="onSubmit"
+                class="bg-success text-white py-2 px-5 text-base float-right my-3 rounded"
+              >
                 {{ $t('actions.create') }}
               </n-button>
             </n-form-item>
           </n-form>
-          <n-space class="text-black-900"><n-switch v-model:value="formValue.isPrivate" />
-            {{ $t('channels.set_channel_private') }}</n-space>
-        </div>
-        <button type="button" class="focus:outline-none bg-none py-1 px-2 text-base float-right text-black-900 rounded"
-          @click="closeModal">
-          <font-awesome-icon icon="fa-solid fa-xmark" />
-        </button>
-        </header>
-        <div class="m-0 relative mt-5">
-          <div class="mb-6 text-black-500">
-            <p>
-              {{ $t('channels.add_channel_dialog') }}
-            </p>
-          </div>
-          <n-form :label-width="80" :model="formValue" :rules="rules">
-            <n-form-item :label="$t('channels.new_channel_name')" path="channelName">
-              <n-input v-model:value="formValue.channelName" :maxlength="80" :count-graphemes="countGraphemes" show-count
-                :placeholder="$t('channels.new_channel_name_placeholder')" />
-            </n-form-item>
-            <n-form-item class="mt-3" :label="$t('channels.new_channel_description')" path="Description">
-              <n-input v-model:value="formValue.Description" :maxlength="500" :count-graphemes="countGraphemes" show-count
-                :placeholder="$t('channels.new_channel_desc_placeholder')" />
-            </n-form-item>
-            <p v-if="error" class="mt-3 text-danger">{{ error }}</p>
-            <n-form-item class="float-right">
-              <n-button @click="onSubmit" class="bg-success text-white py-2 px-5 text-base float-right my-3 rounded">
-                {{ $t('actions.create') }}
-              </n-button>
-            </n-form-item>
-          </n-form>
-          <n-space class="text-black-900"><n-switch v-model:value="formValue.isPrivate" />
-            {{ $t('channels.set_channel_private') }}</n-space>
+          <n-space class="text-black-900"
+            ><n-switch v-model:value="formValue.isPrivate" />
+            {{ $t('channels.set_channel_private') }}</n-space
+          >
         </div>
       </div>
     </div>
